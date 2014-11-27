62689dda9ca6ec4f58c7bf564cbc1690f652a137
Lukas Schrangl <schrangl@gmx.net>
2014-11-27 10:51:39 +0100

    spe_to_tiff.py: Push SPE information into TIFF ImageDescription
    Do so .ini-style s.t. it is human and machine readable.

--- spe_to_tiff.py
+++ spe_to_tiff.py
@@ -6,20 +6,43 @@ Created on Tue Nov 18 11:42:46 2014
 """
 
 import os
+import configparser
+import io
 import OpenImageIO as oiio
+
+def spe_attributes_to_string(inSpec):
+    config = configparser.ConfigParser()
+    config.add_section("Comments")
+    config["Comments"]["Comment1"] = inSpec.get_attribute("Comment1")
+    config["Comments"]["Comment2"] = inSpec.get_attribute("Comment2")
+    config["Comments"]["Comment3"] = inSpec.get_attribute("Comment3")
+    config["Comments"]["Comment4"] = inSpec.get_attribute("Comment4")
+    config["Comments"]["Comment5"] = inSpec.get_attribute("Comment5")
+    config.add_section("ROI")
+    config["ROI"]["startx"] = str(inSpec.get_attribute("ROI_startx"))
+    config["ROI"]["endx"] = str(inSpec.get_attribute("ROI_endx"))
+    config["ROI"]["groupx"] = str(inSpec.get_attribute("ROI_groupx"))
+    config["ROI"]["starty"] = str(inSpec.get_attribute("ROI_starty"))
+    config["ROI"]["endy"] = str(inSpec.get_attribute("ROI_endy"))
+    config["ROI"]["groupy"] = str(inSpec.get_attribute("ROI_groupy"))
+    config.add_section("Laser modulation")
+    config["Laser modulation"]["script"] = inSpec.get_attribute("Spare_4")
+    strio = io.StringIO()
+    config.write(strio)
+    return strio.getvalue()
 
 def convert_file(ifname, ofname):
     oiio.ImageInput.create(ifname, "../spe/spe.imageio/build/")
     input = oiio.ImageInput.open(ifname)
 
-    outSpec = input.spec()
     output = oiio.ImageOutput.create(ofname)
 
     i = 0
     while input.seek_subimage(i, 0):
-        subspec = input.spec()
+        subspec = oiio.ImageSpec(input.spec())
         if i == 0:
             openmode = oiio.Create
+            subspec.attribute("ImageDescription", spe_attributes_to_string(input.spec()))
         else:
             openmode = oiio.AppendSubimage
 
