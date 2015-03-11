063467733494fbe8f19a9064e82e93de0c1696da
Lukas Schrangl <lukas.schrangl@tuwien.ac.at>
2015-03-12 00:43:18 +0100

    chromatic: Create Corrector class
    This should be an easy-to-use interface to everything in chromatic.

--- sdt/chromatic.py
+++ sdt/chromatic.py
@@ -1,3 +1,19 @@
+"""Correct chromatic aberrations of localizations.
+
+When doing multi-color single molecule microscopy, chromatic aberrations pose
+a problem. To circumvent this, one can record an image of beads, match the
+localizations (as determined by a localization algorithm such as trackpy or
+tracking2d) of the two channels and determine affine transformations for the
+x and y coordinates.
+
+Attributes:
+    pos_colums (list of str): Names of the columns describing the x and the y
+        coordinate of the features in pandas.DataFrames. Defaults to
+        ["x", "y"].
+    feat_names (list of str): Names of the two channels. Defaults to
+        ["channel1", "channel2"].
+"""
+
 import numpy as np
 import pandas as pd
 import scipy as sp
@@ -5,6 +21,124 @@ import scipy as sp
 
 pos_columns = ["x", "y"]
 feat_names = ["channel1", "channel2"]
+
+
+class Corrector(object):
+    """Convenience class for easy correction of chromatic aberrations
+
+    This class provides an easy-to-use interface to the correction
+    process.
+
+    Attributes:
+        pos_columns (list of str): Names of the columns describing the x and
+            the y coordinate of the features.
+        feat_names (list of str): Names of the channels.
+        feat1 (pandas.DataFrame): Features of the first channel found by the
+            localization algorithm. The x coordinate is in the column with name
+            `pos_columns`[0], the y coordinate in `pos_columns`[1].
+        feat2 (pandas.DataFrame): Features of the second channel found by the
+            localization algorithm. The x coordinate is in the column with name
+            `pos_columns`[0], the y coordinate in `pos_columns`[1].
+        pairs (pandas.DataFrame): Contains the pairs found by
+            `determine_parameters`.
+        parameters (pandas.DataFrame): The parameters for the transformation as
+            determined by `determine_parameters`.
+    """
+    def __init__(self, feat1, feat2, pos_columns=pos_columns,
+                 feat_names=feat_names):
+        """Constructor
+
+        Args:
+            feat1 (pandas.DataFrame): Sets the `feat1` attribute
+            feat2 (pandas.DataFrame): Sets the `feat2` attribute
+            pos_columns (list of str): Sets the `pos_columns` attribute.
+                Defaults to the `pos_columns` attribute of the module.
+            feat_names (list of str): Sets the `feat_names` attribute.
+                Defaults to the `feat_names` attribute of the module.
+        """
+        self.feat1 = feat1
+        self.feat2 = feat2
+        self.pos_columns = pos_columns
+        self.feat_names = feat_names
+        self.pairs = None
+        self.parms = None
+
+    def determine_parameters(self, tol_rel=0.1, tol_abs=0.):
+        """Determine the parameters for the affine transformation
+
+        This takes the localizations of `feat1` and tries to match them with
+        those of `feat2`. This is done by calculating the vectors from every
+        feature all the other features in `feat1` and compare them to those of
+        `feat2` using the `numpy.isclose` function on both the x and the y
+        coordinate, where `tol_rel` and `tol_abs` are the relative and
+        absolute tolerance parameters.
+
+        Args:
+            tol_rel (float): Relative tolerance parameter for
+                `numpy.isclose()`. Defaults to 0.1.
+            tol_abs (float): Absolute tolerance parameter for
+                `numpy.isclose()`. Defaults to 0.
+        """
+        #TODO: score_cutoff
+        self.pairs = find_pairs(self.feat1, self.feat2, tol_rel, tol_abs,
+                                None, self.pos_columns, self.feat_names)
+        self.parameters = correction_parameters(self.pairs, self.pos_columns,
+                                                self.feat_names)
+
+    def __call__(self, features, channel=2):
+        """Do chromatic correction on `features` coordinates
+
+        This modifies the coordinates in place.
+
+        Args:
+            features (pandas.DataFrame): The features to be corrected
+            channel (int, optional): If `features` are in the first channel
+                (corresponding to the `feat1` arg of the constructor), set to
+                1. If features are in the second channel, set to 2. Depending
+                on this, a transformation will be applied to the coordinates of
+                `features` to match the other channel (mathematically speaking
+                depending on this parameter either the "original" transformation
+                or its inverse are applied.)
+        """
+        if channel != 1 and channel != 2:
+            raise ValueError("channel has to be either 1 or 2")
+
+        x_col = self.pos_columns[0]
+        y_col = self.pos_columns[1]
+        xparm = self.parameters.loc[x_col]
+        yparm = self.parameters.loc[y_col]
+
+        if channel == 1:
+            features[x_col] = features[x_col]*xparm.slope + xparm.intercept
+            features[y_col] = features[y_col]*yparm.slope + yparm.intercept
+
+        if channel == 2:
+            features[x_col] = (features[x_col] - xparm.intercept)/xparm.slope
+            features[y_col] = (features[y_col] - yparm.intercept)/yparm.slope
+
+
+    def test(self):
+        """Test validity of the correction parameters
+
+        This plots the affine transformation functions and the coordinates of
+        the pairs that were matched in the channels. If everything went well,
+        the dots (i. e. pair coordinates) should lie on the line
+        (transformation function).
+        """
+        import matplotlib.pyplot as plt
+
+        for i, p in enumerate(self.pos_columns):
+            plt.subplot(1, len(self.pos_columns), i+1, aspect=1)
+            ax = plt.gca()
+            ax.set_xlabel("{} ({})".format(p, self.feat_names[0]))
+            ax.set_ylabel("{} ({})".format(p, self.feat_names[1]))
+            ax.scatter(self.pairs[self.feat_names[0], p],
+                       self.pairs[self.feat_names[1], p])
+            ax.plot(self.pairs[self.feat_names[0]].sort(p)[p],
+                    self.parameters.loc[p, "slope"] *
+                    self.pairs[self.feat_names[0]].sort(p)[p] +
+                    self.parameters.loc[p, "intercept"])
+            ax.set_title(p)
 
 
 def vectors_cartesian(features, pos_columns = pos_columns):
