806423ef0170554a0611750eb65af63e684f7928
Lukas Schrangl <lukas.schrangl@tuwien.ac.at>
2015-06-22 17:11:55 +0200

    brightness: Use a DataFrame.apply() based approach for better performance

--- sdt/brightness.py
+++ sdt/brightness.py
@@ -14,6 +14,7 @@ Attributes:
         Defaults to "background".
 """
 import numpy as np
+import pandas as pd
 
 
 pos_columns = ["x", "y"]
@@ -22,38 +23,48 @@ mass_column = "mass"
 bg_column = "background"
 
 
-def get_raw_brightness(positions, frames, diameter=5, bg_frame=2,
+def _get_raw_brightness_single(data, frames, diameter=5, bg_frame=1,
+                               pos_columns=pos_columns,
+                               t_column=t_column, mass_column=mass_column,
+                               bg_column=bg_column):
+    frameno = int(data[t_column])
+    pos = np.round(data[pos_columns])
+    ndim = len(pos_columns)
+    fr = frames[frameno]
+    sz = np.floor(diameter/2.)
+    start = pos - sz - bg_frame
+    end = pos + sz + bg_frame + 1
+
+    signal_region = fr[ [slice(s, e) for s, e in zip(reversed(start),
+                                                         reversed(end))] ]
+
+    if (signal_region.shape != end - start).any():
+        mass = np.NaN
+        background_intensity = np.NaN
+    elif bg_frame == 0 or bg_frame is None:
+        mass = signal_region.sum()
+        background_intensity = 0
+    else:
+        signal_slice = [slice(bg_frame, -bg_frame)]*ndim
+        uncorr_intensity = signal_region[signal_slice].sum()
+        #TODO: threshold uncorr intensity?
+        signal_region[signal_slice] = 0
+        background_pixels = signal_region[signal_region.nonzero()]
+        background_intensity = np.mean(background_pixels)
+        mass = uncorr_intensity - background_intensity * diameter**ndim
+
+    return pd.Series({mass_column: mass,
+                      bg_column: background_intensity})
+
+
+def get_raw_brightness(positions, frames, diameter=5, bg_frame=1,
                        pos_columns=pos_columns,
                        t_column=t_column, mass_column=mass_column,
                        bg_column=bg_column):
-    p = np.round(positions[[t_column] + pos_columns])
-    sz = np.floor(diameter/2.)
-    ndim = len(pos_columns)
+    brightness = positions[[t_column] + pos_columns].apply(
+        _get_raw_brightness_single,
+        args=(frames, diameter, bg_frame, pos_columns,
+              t_column, mass_column, bg_column),
+        axis=1)
 
-    for r in p.iterrows():
-        r = r[1]
-        fr = frames[int(r[t_column])]
-        start = r[pos_columns] - sz - bg_frame
-        end = r[pos_columns] + sz + bg_frame + 1
-
-        signal_region = fr[ [slice(s, e) for s, e in zip(reversed(start),
-                                                         reversed(end))] ]
-        if (signal_region.shape != end - start).any():
-            positions.loc[r.name, bg_column] = np.NaN
-            positions.loc[r.name, mass_column] = np.NaN
-            continue
-
-        if bg_frame == 0 or bg_frame is None:
-            uncorr_intensity = signal_region.sum()
-            background_intensity = 0
-        else:
-            signal_slice = [slice(bg_frame, -bg_frame)]*ndim
-            uncorr_intensity = signal_region[signal_slice].sum()
-            #TODO: threshold uncorr intensity?
-            signal_region[signal_slice] = 0
-            background_pixels = signal_region[signal_region.nonzero()]
-            background_intensity = np.mean(background_pixels)
-
-        positions.loc[r.name, bg_column] = background_intensity
-        positions.loc[r.name, mass_column] = (uncorr_intensity -
-            background_intensity * diameter**ndim)
+    positions[brightness.columns] = brightness
