8138441c91485ee43c15446f0c866774edbc2130
Lukas Schrangl <lukas.schrangl@tuwien.ac.at>
2015-07-16 10:54:30 +0200

    pims_spe: Implement metadata reading
    Only some metadata is read (only that actually written by the SDTcontrol
    software), but the mechanism should be easily extensible.
    
    Also use now numpy.fromfile exclusively to read data.

--- sdt/pims_spe.py
+++ sdt/pims_spe.py
@@ -1,5 +1,3 @@
-from struct import calcsize, unpack
-
 import numpy as np
 
 from pims import FramesSequence, Frame
@@ -8,19 +6,39 @@ from pims import FramesSequence, Frame
 class spec(object):
     """SPE file specification data
 
-    Tuples of (offset, datatype), where offset is the offset in the SPE
+    Tuples of (offset, datatype, count), where offset is the offset in the SPE
     file and datatype is a string describing the datatype as used in
-    `struct.unpack`()
+    `numpy.fromfile`()
 
     `data_start` is the offset of actual image data.
 
     `dtypes` translates SPE datatypes (0...4) to numpy ones, e. g. dtypes[0]
     is dtype("<f") (which is np.float32).
     """
-    datatype = (108, "h")
-    xdim = (42, "H")
-    ydim = (656, "H")
-    numframes = (1446, "i")
+    datatype = (108, "<h")
+    xdim = (42, "<H")
+    ydim = (656, "<H")
+    numframes = (1446, "<i")
+
+    metadata = {
+        "comments": (200, "<80S", 5),
+        "spare4": (742, "<436S"),
+
+        "swversion": (688, "<16S"),
+        "header_version": (1992, "<f"),
+
+        "date": (20, "<10S"),
+        "local_time": (172, "<7S"),
+        "utc_time": (179, "<7S"),
+
+        "ROIs": (1512, np.dtype([("startx", "<H"),
+                                 ("endx", "<H"),
+                                 ("groupx", "<H"),
+                                 ("starty", "<H"),
+                                 ("endy", "<H"),
+                                 ("groupy", "<H")]), 5)
+    }
+    num_rois = (1510, "<h")
 
     data_start = 4100
 
@@ -29,19 +47,37 @@ class spec(object):
 
 
 class SpeStack(FramesSequence):
+    """Read image data from SPE files
+
+    Attributes:
+        metadata (dict): Contains additional metadata.
+    """
     @classmethod
     def class_exts(cls):
         return {"spe"} | super(SpeStack, cls).class_exts()
 
     def __init__(self, filename, process_func=None, dtype=None,
-                 as_grey=False):
+                 as_grey=False, char_encoding="ascii"):
+        """Create an iterable object that returns image data as numpy arrays
+
+        Args:
+            filename (str): Name of the SPE file
+            process_func (callable, optional): Takes one image array as its
+                sole argument. It is applied to each image. Defaults to None.
+            dtype (numpy.dtype, optional): Which data type to convert the
+                images too. No conversion if None. Defaults to None.
+            as_grey (bool, optional): Convert image to greyscale. Do not use
+                in conjunction with process_func. Defaults to False.
+            char_encoding (str, optional): Specifies what character encoding
+                is used for metatdata strings. Defaults to "ascii".
+        """
         self._filename = filename
         self._file = open(filename, "rb")
+        self._char_encoding = char_encoding
 
         #determine data type
         self._file.seek(spec.datatype[0])
-        b = self._file.read(calcsize(spec.datatype[1]))
-        d, = unpack("<{}".format(spec.datatype[1]), b)
+        d = np.fromfile(self._file, spec.datatype[1], count=1)
         self._file_dtype = spec.dtypes[d]
 
         if dtype is None:
@@ -51,17 +87,52 @@ class SpeStack(FramesSequence):
 
         #movie dimensions
         self._file.seek(spec.xdim[0])
-        b = self._file.read(calcsize(spec.xdim[1]))
+        self._width = np.fromfile(self._file, spec.xdim[1], count=1)[0]
         self._file.seek(spec.ydim[0])
-        b += self._file.read(calcsize(spec.ydim[1]))
+        self._height = np.fromfile(self._file, spec.ydim[1], count=1)[0]
         self._file.seek(spec.numframes[0])
-        b += self._file.read(calcsize(spec.numframes[1]))
-        self._width, self._height, self._len = unpack(
-            "<{}{}{}".format(spec.xdim[1], spec.ydim[1], spec.numframes[1]),
-            b)
+        self._len = np.fromfile(self._file, spec.numframes[1], count=1)[0]
 
+        #read additional metadata
+        self.metadata = {}
+        self._read_metadata()
+
+        #pims-specific stuff
         self._validate_process_func(process_func)
         self._as_grey(as_grey, process_func)
+
+    def _read_metadata(self):
+        """Actual reading of the additional metadata
+
+        Metadata gets written to self.metadata. Strings are decoded to
+        python strings using the character encoding self._char_encoding
+        """
+        #Decode each string from the numpy array read by np.fromfile
+        #function definition
+        decode = np.vectorize(lambda x: x.decode(self._char_encoding))
+
+        for name, sp in spec.metadata.items():
+            self._file.seek(sp[0])
+            cnt = (1 if len(sp) < 3 else sp[2])
+            v = np.fromfile(self._file, dtype=sp[1], count=cnt)
+            if v.dtype.kind == "S":
+                #silently ignore string decoding failures
+                try:
+                    v = decode(v)
+                except:
+                    pass
+            if cnt == 1:
+                #for convenience, if the array contains only one single entry,
+                #return this entry itself.
+                v = np.asscalar(v)
+            self.metadata[name] = v
+
+        #The number of ROIs is specified in the SPE file. Only return as many
+        #ROIs as specified
+        self._file.seek(spec.num_rois[0])
+        num_rois = np.fromfile(self._file, spec.num_rois[1], count=1)
+        num_rois = (1 if num_rois < 1 else num_rois)
+        self.metadata["ROIs"] = self.metadata["ROIs"][:num_rois]
 
     @property
     def frame_shape(self):
