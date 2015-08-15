f641eebce9aaaa145a0f60a921f11815e2b3c9e0
Lukas Schrangl <lukas.schrangl@tuwien.ac.at>
2015-08-15 14:36:00 +0200

    compat: Cosmetic changes
    docstrings in numpy format, PEP8 improvements, etc.

--- sdt/compat.py
+++ sdt/compat.py
@@ -6,31 +6,34 @@ This module provides a compatibility layer for various tools written in
 MATLAB. So far it can read data produced by
 - particle_tracking_2D
 - prepare_peakposition (and anything als that produces pk files)
+- check_fit (i. e. pks files)
 - msdplot.
 
-Attributes:
-    pt2d_name_trans: `collections.OrderedDict` that contains as keys the
-        names of the columns of particle_tracking_2D output .mat files as
-        found in the _protocol.mat files, and as values something shorter
+Attributes
+----------
+    pt2d_name_trans : collections.OrderedDict
+        Keys are the names of the columns of particle_tracking_2D output .mat
+        files as found in the _protocol.mat files. Values are something shorter
         that can be handled better and, by default, is compatible with
-        `trackpy`.
-    pk_column_names: List of the names of the columns of a pk matrix as
-        produce e. g. by prepare_peakposition. By default also compatible
-        with trackpy.
-    pks_column_names: List of the names of the columns of a pks matrix as
-        produce e. g. by `check_fit`. By default also compatible
-        with trackpy.
-    msd_column_names: List of the names of the columns of a msdplot output
-        matrix.
-    mass_column (str): Name of the column describing the integrated intensities
-        ("masses") of the features. Defaults to "mass".
+        [trackpy](https://soft-matter.github.io/trackpy/).
+    pk_column_names : list of st
+        Names of the columns of a pk matrix as produced e. g. by
+        ``prepare_peakposition``. By default also compatible with trackpy.
+    pks_column_names : list of str
+        Names of the columns of a pks matrix as produced e. g. by
+        ``check_fit``. By default also compatible with trackpy.
+    msd_column_names : list of str
+        Names of the columns of a msdplot output matrix.
+    mass_column : str
+        Name of the column describing the integrated intensities ("masses") of
+        the features. Defaults to "mass".
 """
+import collections
 
 import scipy.io as sp_io
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import collections
+
 
 pt2d_name_trans = collections.OrderedDict((
     ("x-Position", "x"),
@@ -46,16 +49,12 @@ pt2d_name_trans = collections.OrderedDict((
     ("Frame Number", "frame"),
     ("Time in Frames", "time"),
     ("Trace ID", "particle")))
-
 pk_column_names = ["frame", "x", "y", "size", "mass", "background",
                    "column6", "column7", "background_dev", "column9",
                    "column10"]
-
 pks_column_names = ["frame", "x", "y", "size", "mass", "background",
                     "background_dev", "ep"]
-
 msd_column_names = ["tlag", "msd", "stderr", "qianerr"]
-
 mass_column = "mass"
 
 
@@ -66,19 +65,24 @@ def load_pt2d_positions(filename, load_protocol=True,
     Use `scipy.io.loadmat` to load the file and convert data to a
     `pandas.DataFrame`.
 
-    Args:
-        filename (str): Name of the file to load
-        load_protocol (bool): Look for a _protocol.mat file (i. e. replace the
-            "_positions.mat" part of `filename` with "_protocol.mat") in order
-            to load the column names. This may be buggy for some older versions
-            of particle_tracking_2D!
-        adjust_index (list of str): Since MATLAB's indices start at 1 and
-            python's start at 0, some data (such as feature coordinates and
-            frame numbers) may be off by one. This list contains the names of
-            columns to be corrected for this. Defaults to ["x", "y", "frame"].
+    Parameters
+    ----------
+    filename : str
+        Name of the file to load
+    load_protocol : bool
+        Look for a _protocol.mat file (i. e. replace the "_positions.mat" part
+        of `filename` with "_protocol.mat") in order to load the column names.
+        This may be buggy for some older versions of particle_tracking_2D.
+    adjust_index : list of str
+        Since MATLAB's indices start at 1 and python's start at 0, some data
+        (such as feature coordinates and frame numbers) may be off by one.
+        This list contains the names of columns to be corrected. Defaults to
+        ["x", "y", "frame"].
 
-    Returns:
-        pandas.DataFrame containing the data.
+    Returns
+    -------
+    pandas.DataFrame
+        Loaded data.
     """
     pos = sp_io.loadmat(filename)["MT"]
 
@@ -125,20 +129,24 @@ def load_pt2d_tracks(filename, load_protocol=True,
     Use `scipy.io.loadmat` to load the file and convert data to a
     `pandas.DataFrame`.
 
-    Args:
-        filename (str): Name of the file to load
-        load_protocol (bool): Look for a _protocol.mat file (i. e. replace the
-            "_tracks.mat" part of `filename` with "_protocol.mat") in order
-            to load the column names. This may be buggy for some older versions
-            of particle_tracking_2D!
-        adjust_index (list of str): Since MATLAB's indices start at 1 and
-            python's start at 0, some data (such as feature coordinates and
-            frame numbers) may be off by one. This list contains the names of
-            columns to be corrected for this. Defaults to ["x", "y", "frame",
-            "particle"].
+    Parameters
+    ----------
+    filename : str
+        Name of the file to load
+    load_protocol : bool
+        Look for a _protocol.mat file (i. e. replace the "_positions.mat" part
+        of `filename` with "_protocol.mat") in order to load the column names.
+        This may be buggy for some older versions of particle_tracking_2D.
+    adjust_index : list of str
+        Since MATLAB's indices start at 1 and python's start at 0, some data
+        (such as feature coordinates and frame numbers) may be off by one.
+        This list contains the names of columns to be corrected. Defaults to
+        ["x", "y", "frame", "particle"].
 
-    Returns:
-        pandas.DataFrame containing the data.
+    Returns
+    -------
+    pandas.DataFrame
+        Loaded data.
     """
     tracks = sp_io.loadmat(filename)["tracks"]
 
@@ -178,24 +186,34 @@ def load_pt2d_tracks(filename, load_protocol=True,
     return df
 
 
-def load_pkmatrix(filename, adjust_index=["x", "y", "frame"], green=False):
+def load_pkmatrix(filename, green=False, adjust_index=["x", "y", "frame"]):
     """Load a pkmatrix from a .mat file
 
     Use `scipy.io.loadmat` to load the file and convert data to a
     `pandas.DataFrame`.
 
-    Args:
-        filename (str): Name of the file to load
-        adjust_index (list of str): Since MATLAB's indices start at 1 and
-            python's start at 0, some data (such as feature coordinates and
-            frame numbers) may be off by one. This list contains the names of
-            columns to be corrected for this. Defaults to ["x", "y", "frame"].
-        green (bool): If True, load pkmatrix_green, which is the right half
-            of the image when using `prepare_peakposition` in 2 color LR mode.
-            Otherwise, load pkmatrix. Defaults to False.
+    Parameters
+    ----------
+    filename : str
+        Name of the file to load
+    load_protocol : bool
+        Look for a _protocol.mat file (i. e. replace the "_positions.mat" part
+        of `filename` with "_protocol.mat") in order to load the column names.
+        This may be buggy for some older versions of particle_tracking_2D.
+    green : bool
+        If True, load ``pkmatrix_green``, which is the right half of the image
+        when using ``prepare_peakposition`` in 2 color mode. Otherwise, load
+        ``pkmatrix``. Defaults to False.
+    adjust_index : list of str
+        Since MATLAB's indices start at 1 and python's start at 0, some data
+        (such as feature coordinates and frame numbers) may be off by one.
+        This list contains the names of columns to be corrected. Defaults to
+        ["x", "y", "frame"].
 
-    Returns:
-        pandas.DataFrame containing the data.
+    Returns
+    -------
+    pandas.DataFrame
+        Loaded data.
     """
     mat = sp_io.loadmat(filename, struct_as_record=False, squeeze_me=True)
 
@@ -227,15 +245,28 @@ def load_pks(filename, adjust_index=["x", "y", "frame"]):
     Use `scipy.io.loadmat` to load the file and convert data to a
     `pandas.DataFrame`.
 
-    Args:
-        filename (str): Name of the file to load
-        adjust_index (list of str): Since MATLAB's indices start at 1 and
-            python's start at 0, some data (such as feature coordinates and
-            frame numbers) may be off by one. This list contains the names of
-            columns to be corrected for this. Defaults to ["x", "y", "frame"].
+    Parameters
+    ----------
+    filename : str
+        Name of the file to load
+    load_protocol : bool
+        Look for a _protocol.mat file (i. e. replace the "_positions.mat" part
+        of `filename` with "_protocol.mat") in order to load the column names.
+        This may be buggy for some older versions of particle_tracking_2D.
+    green : bool
+        If True, load ``pkmatrix_green``, which is the right half of the image
+        when using ``prepare_peakposition`` in 2 color mode. Otherwise, load
+        ``pkmatrix``. Defaults to False.
+    adjust_index : list of str
+        Since MATLAB's indices start at 1 and python's start at 0, some data
+        (such as feature coordinates and frame numbers) may be off by one.
+        This list contains the names of columns to be corrected. Defaults to
+        ["x", "y", "frame"].
 
-    Returns:
-        pandas.DataFrame containing the data.
+    Returns
+    -------
+    pandas.DataFrame
+        Loaded data.
     """
     mat = sp_io.loadmat(filename, struct_as_record=False, squeeze_me=True)
 
@@ -265,19 +296,25 @@ def plotpdf(data, lim, f, matlab_engine, ax=None, mass_column=mass_column):
 
     This is a wrapper around `plotpdf` using MATLAB engine for python
 
-    Args:
-        data (list or pandas.DataFrame): List of molecule brightnesses. If it
-            is a DataFrame, use the column named by the `mass_column` argument.
-        lim (float): Maximum brightness
-        f (float): Correction factor for sigma
-        matlab_engine: MATLAB engine object (as returned by
-            `matlab.engine.start()`)
-        ax (optional): `matplotlib` axes to use for plotting. If None, use
-            `gca()`. Defaults to None.
-        mass_column (str, optional): Name of the column describing the
-            integrated intensities ("masses") of the features. Defaults to the
-            `mass_column` attribute of the module.
+    Parameters
+    ----------
+    data : list or pandas.DataFrame
+        List of molecule brightnesses. If it is a DataFrame, use the column
+        named by the `mass_column` argument.
+    lim : float
+        Maximum brightness
+    f : float
+        Correction factor for sigma
+    matlab_engine : MATLAB engine object
+        as e.g. returned by `matlab.engine.start()`
+    ax : axis object, optional
+        `matplotlib` axes to use for plotting. If None, use `gca()`. Defaults
+        to None.
+    mass_column : str, optional
+        Name of the column describing the integrated intensities ("masses") of
+        the features. Defaults to the `mass_column` attribute of the module.
     """
+    import matplotlib.pyplot as plt
     import matlab.engine
 
     if ax is None:
@@ -287,21 +324,21 @@ def plotpdf(data, lim, f, matlab_engine, ax=None, mass_column=mass_column):
         data = data[mass_column]
 
     data = [[d] for d in data]
-    x, y =  matlab_engine.plotpdf(matlab.double(data), float(lim), float(f),
+    x, y = matlab_engine.plotpdf(matlab.double(data), float(lim), float(f),
                                   "r", 0, nargout=2)
     ax.plot(x._data, y._data)
 
 
-def load_msdplot(filename, column_names=msd_column_names):
+def load_msdplot(filename):
     """Load msdplot data from .mat file
 
-    Args:
-        filename (str): Name of the file to load
-        column_names (list of str): List of the column names. Defaults to
-            `msd_column_names`.
+    Parameters
+    ----------
+    filename : str
+        Name of the file to load
 
     Returns:
-        A dict with keys: d, stderr, qianerr, pa, data.
+    dict([d, stderr, qianerr, pa, data])
         d is the diffusion coefficient in μm²/s, stderr its standard
         error, qianerr its Qian error, pa the positional accuracy in nm and
         data a pandas.DataFrame containing the msd-vs.-tlag data.
@@ -311,4 +348,4 @@ def load_msdplot(filename, column_names=msd_column_names):
                 stderr=mat["dstd1"],
                 qianerr=mat["dstd_qian1"],
                 pa=mat["pos1"],
-                data=pd.DataFrame(mat["msd1"], columns=column_names))
+                data=pd.DataFrame(mat["msd1"], columns=msd_column_names))
