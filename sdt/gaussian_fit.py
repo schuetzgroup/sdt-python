76855ce75acb88e3b7032181da244eca6371c6bd
Lukas Schrangl <lukas.schrangl@tuwien.ac.at>
2015-11-21 21:17:48 +0100

    Rework gaussian_fit
    Benefits:
    - nicer API
    - more robustness (e. g. don't weigh error function with a factor 1/r, which
      may become infinite for r = 0)
    - may be extended to arbitrary dimensions (currently only gaussian() is
      restricted to 1D or 2D).

--- sdt/gaussian_fit.py
+++ sdt/gaussian_fit.py
@@ -6,8 +6,112 @@
 
 from __future__ import division
 import numpy as np
-import numpy.ma
 import scipy.optimize
+
+def guess_paramaters(data):
+    # median of the edges as an estimate for the background
+    interior_slice = slice(1, -1)
+    edge_mask = np.ones(data.shape, dtype=np.bool)
+    edge_mask[[interior_slice]*data.ndim] = False
+    bg = np.median(data[edge_mask])
+
+    # subtract background for calculation of moments, mask negative values
+    data_bg = np.ma.masked_less(data - bg, 0)
+    data_bg_sum = np.sum(data_bg)
+
+    # maximum as an estimate for the amplitude
+    amp = np.max(data_bg)
+
+    # calculate 1st moments as estimates for centers
+    all_idx = np.indices(data.shape)
+    center = np.fromiter((np.sum(i * data_bg)/data_bg_sum for i in all_idx),
+                         dtype=np.float)
+
+    # calculate 2nd moments along lines through center as estimates for sigma
+    all_slice = slice(0, None)  # selects all
+    sigma = []
+    for i, ci in enumerate(center):
+        c = center.astype(np.int).tolist()  # indices closest to the center
+        c[i] = all_slice  # only along the i-th coordinate select whole line
+        line = data_bg[tuple(c)]
+        idx = np.arange(len(line))
+        sigma.append(np.sqrt(np.sum((line*(idx - ci)**2))/np.sum(line)))
+    sigma = np.array(sigma)
+
+    # for 2D data only: Use inertia matrix to determine angle of rotation
+    angle = 0.
+    if data.ndim == 2:
+        m_xx = np.sum(data_bg*(all_idx[0]-center[0])**2) / data_bg_sum
+        m_xy = (np.sum(data_bg*(all_idx[0]-center[0])*(all_idx[1]-center[1])) /
+                data_bg_sum)
+        m_yy = np.sum(data_bg*(all_idx[1]-center[1])**2) / data_bg_sum
+        angle = 0.5 * np.arctan(2*m_xy / (m_xx - m_yy))
+
+    return dict(amplitude=amp, center=center, sigma=sigma, background=bg,
+                angle=angle)
+
+
+def gaussian(amplitude, center, sigma, background, angle=0.):
+    ndim = len(center)
+    if np.isscalar(sigma):
+        sigma = [sigma]*ndim
+    if len(sigma) != ndim:
+        raise ValueError("sigma needs to be a number or the number of sigma "
+                         "values needs to match the number of center entries.")
+
+    if ndim == 1:
+        def gaussian_1d(x):
+            return (amplitude * np.exp(-((x - center[0])/sigma[0])**2/2.) +
+                    background)
+        return gaussian_1d
+    elif ndim == 2:
+        cs = np.cos(angle)
+        sn = np.sin(angle)
+
+        def rotate(x, y):
+            return x*cs - y*sn, x*sn + y*cs
+
+        xc_r, yc_r = rotate(center[0], center[1])
+        def gaussian2d(x, y):
+            x_r, y_r = rotate(x, y)
+            arg = ((x_r - xc_r)/sigma[0])**2 + ((y_r - yc_r)/sigma[1])**2
+            return (amplitude * np.exp(-arg/2.) + background)
+        return gaussian2d
+    else:
+        raise ValueError("Only 1D and 2D Gaussians are supported at the"
+                         "moment")
+
+
+def fit(data, initial_guess=None):
+    if initial_guess is None:
+        initial_guess = guess_paramaters(data)
+
+    all_idx = np.indices(data.shape)
+    ndim = data.ndim
+
+    def param_tuple_to_dict(params):
+        return dict(amplitude=params[0], center=np.array(params[1:ndim+1]),
+                    sigma=np.array(params[ndim+1:2*ndim+1]),
+                    background=params[2*ndim + 1], angle=params[2*ndim + 2])
+
+    def err_func(params):
+        params = param_tuple_to_dict(params)
+        curr_gauss = gaussian(**params)
+        err = curr_gauss(*all_idx) - data
+        # alternative: weigh the error
+        return np.ravel(err)
+
+    # scipy.optimize.leastsq expects a list of parameters. For this, flatten
+    # the parameters structure
+    initial_list = ([initial_guess["amplitude"]] +
+                    initial_guess["center"].tolist() +
+                    initial_guess["sigma"].tolist() +
+                    [initial_guess["background"], initial_guess["angle"]])
+    p, success = scipy.optimize.leastsq(err_func, initial_list)
+
+    if success > 2:
+        raise RuntimeError("Fit did not converge.")
+    return param_tuple_to_dict(p)
 
 #------------------------------------1D gaussian fitting functions...
 def moments1D(inpData):
@@ -54,7 +158,7 @@ def FitGauss1D(Data,ip=None):
     def errfun(ip):
         return np.ravel(Gaussian1D(*ip)(*np.indices(Data.shape)) - Data)
 
-    p, success = scipy.optimize.leastsq(errfun, ip)
+    p, success = scipy.optimize.leastsq(errfun, ip, maxfev=1)
 
     return p,success
 
@@ -64,7 +168,11 @@ def FitGauss1D(Data,ip=None):
 def moments2D(inpData):
     """ Returns the (amplitude, xcenter, ycenter, xsigma, ysigma, rot, bkg, e) estimated from moments in the 2d input array Data """
 
-    bkg=np.median(np.hstack((inpData[0,:],inpData[-1,:],inpData[:,0],inpData[:,-1])))  #Taking median of the 4 edges points as background
+    #bkg=np.median(np.hstack((inpData[0,:],inpData[-1,:],inpData[:,0],inpData[:,-1])))  #Taking median of the 4 edges points as background
+    interior_slice = slice(1, -1)
+    edge_mask = np.ones(inpData.shape, dtype=np.bool)
+    edge_mask[[interior_slice]*inpData.ndim] = False
+    bkg = np.median(inpData[edge_mask])
     Data=np.ma.masked_less(inpData-bkg,0)   #Removing the background for calculating moments of pure 2D gaussian
     #We also masked any negative values before measuring moments
 
--- tests/test_gaussian.py
+++ tests/test_gaussian.py
@@ -14,23 +14,61 @@ path, f = os.path.split(os.path.abspath(__file__))
 data_path = os.path.join(path, "data_gaussian")
 
 
-class TestBeamShape(unittest.TestCase):
+class TestGaussianFit(unittest.TestCase):
     def setUp(self):
-        self.img = np.load(os.path.join(data_path, "2d_img.npy"))
-        self.line = self.img[90, :]
+        self.params_1d = dict(amplitude=10, center=[40], sigma=[15],
+                              background=20, angle=0)
+        self.params_2d = dict(amplitude=10, center=[40, 120], sigma=[40, 15],
+                              background=15, angle=np.deg2rad(30))
 
-    def test_moments_1D(self):
-        # This is what the original implementation from HandyTools4Astronomers
-        # returned
-        # amplitude, center, sigma, bkgC, bkgSlope
-        orig = np.load(os.path.join(data_path, "moments_1d.npy"))
-        m = sdt.gaussian_fit.moments1D(self.line)
-        np.testing.assert_allclose(m, orig)
+    def test_gaussian_1d(self):
+        orig = np.load(os.path.join(data_path, "gaussian_1d.npy"))
+        gauss = sdt.gaussian_fit.gaussian(**self.params_1d)
+        x = np.arange(100)
+        np.testing.assert_allclose(gauss(x), orig)
 
-    def test_moments_2D(self):
-        # This is what the original implementation from HandyTools4Astronomers
-        # returned
-        # amplitude, xcenter, ycenter, xsigma, ysigma, rot, bkg, e
-        orig = np.load(os.path.join(data_path, "moments_2d.npy"))
-        m = sdt.gaussian_fit.moments2D(self.img)
-        np.testing.assert_allclose(m, orig)
+    def test_gaussian_2d(self):
+        orig = np.load(os.path.join(data_path, "gaussian_2d.npy"))
+        gauss = sdt.gaussian_fit.gaussian(**self.params_2d)
+        i, j = np.indices((100, 200))
+        np.testing.assert_allclose(gauss(i, j), orig)
+
+    def test_guess_1d(self):
+        data = np.load(os.path.join(data_path, "gaussian_1d.npy"))
+        guess = sdt.gaussian_fit.guess_paramaters(data)
+
+        for k, v in guess.items():
+            # if the guess is within 10% of the actual values, this is good
+            np.testing.assert_allclose(v, self.params_1d[k], rtol=0.1)
+
+    def test_guess_2d(self):
+        data = np.load(os.path.join(data_path, "gaussian_2d.npy"))
+        guess = sdt.gaussian_fit.guess_paramaters(data)
+
+        for k, v in guess.items():
+            if k == "angle":
+                # This can be way off
+                continue
+            if k == "sigma":
+                # also not very good
+                np.testing.assert_allclose(v, self.params_2d[k], rtol=1)
+                continue
+            np.testing.assert_allclose(v, self.params_2d[k], rtol=0.2)
+
+    def test_fit_1d(self):
+        data = np.load(os.path.join(data_path, "gaussian_1d.npy"))
+        fit = sdt.gaussian_fit.fit(data)
+
+        for k, v in fit.items():
+            np.testing.assert_allclose(v, self.params_1d[k], rtol=1e-5)
+
+    def test_fit_2d(self):
+        data = np.load(os.path.join(data_path, "gaussian_2d.npy"))
+        fit = sdt.gaussian_fit.fit(data)
+
+        for k, v in fit.items():
+            np.testing.assert_allclose(v, self.params_2d[k], rtol=1e-5)
+
+
+if __name__ == "__main__":
+    unittest.main()
