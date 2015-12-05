c25031a2c7f6dc8e9d6a8bf390ed8269019f7687
Lukas Schrangl <lukas.schrangl@tuwien.ac.at>
2015-12-05 14:06:48 +0100

    gaussian_fit: Create models for lmfit
    1D and 2D Gaussians

--- sdt/gaussian_fit.py
+++ sdt/gaussian_fit.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """Easy fitting with Gaussian functions
 
 This module allows for fitting Gaussian functions to 1D or 2D data. It is
@@ -13,12 +14,14 @@ Examples
 """
 import numpy as np
 import scipy.optimize
+import lmfit
 
 
-def guess_paramaters(data):
+def guess_paramaters(data, *indep_vars):
     """Initial guess of parameters of the Gaussian
 
     This function does a crude estimation of the parameters:
+
     - The background is guessed by looking at the edges of `data`.
     - The center of the Gaussian is approximated by the center of mass.
     - sigma as well as the angle of rotation are estimated by looking at second
@@ -29,17 +32,21 @@ def guess_paramaters(data):
     ----------
     data : numpy.ndarray
         Data for which the parameter should be guessed
+    *indep_vars : numpy.ndarrays
+        Independent variables (x, y, z, …)
 
     Returns
     -------
     dict
         This dict has the follwing keys:
+
         - "amplitude" (float)
         - "center" (np.ndarray): coordinates of the guessed center
         - "sigma" (np.ndarray)
         - "background" (float)
-        - "angle" (float): guessed angle of rotation. Works (currently) only
+        - "rotation" (float): guessed angle of rotation. Works (currently) only
           for 2D data.
+
         The keys match the arguments of `gaussian` so that the dict can be
         passed directly to the function.
     """
@@ -57,29 +64,30 @@ def guess_paramaters(data):
     amp = np.max(data_bg)
 
     # calculate 1st moments as estimates for centers
-    all_idx = np.indices(data.shape)
-    center = np.fromiter((np.sum(i * data_bg)/data_bg_sum for i in all_idx),
+    center = np.fromiter((np.sum(i * data_bg)/data_bg_sum for i in indep_vars),
                          dtype=np.float)
 
     # Estimate the covariance matrix to determine sigma and the rotation
-    m = np.empty([len(all_idx)]*2)
-    for i in range(len(all_idx)):
+    m = np.empty([len(indep_vars)]*2)
+    for i in range(len(indep_vars)):
         for j in range(i+1):
-            m[i, j] = (np.sum(data_bg * (all_idx[i]-center[i]) *
-                              (all_idx[j]-center[j])) / data_bg_sum)
+            m[i, j] = (np.sum(data_bg * (indep_vars[i]-center[i]) *
+                              (indep_vars[j]-center[j])) / data_bg_sum)
             if i != j:
                 m[j, i] = m[i, j]
     sigma = np.sqrt(m.diagonal())
 
-    angle = 0.
+    angle = None
     if data.ndim == 2:
         angle = 0.5 * np.arctan(2*m[0, 1] / (m[0, 0] - m[1, 1]))
 
-    return dict(amplitude=amp, center=center, sigma=sigma, background=bg,
-                angle=angle)
+    ret = dict(amplitude=amp, center=center, sigma=sigma, background=bg)
+    if angle is not None:
+        ret["rotation"] = angle
+    return ret
 
 
-def gaussian(amplitude, center, sigma, background, angle=0.):
+def gaussian(amplitude, center, sigma, background, rotation=0.):
     """Create a Gaussian function from parameters
 
     This returns a callable that computes Gaussian function values from
@@ -100,7 +108,7 @@ def gaussian(amplitude, center, sigma, background, angle=0.):
         the i-th coordinate axis.
     background : float
         additive offset
-    angle : float, optional
+    rotation : float, optional
         angle of rotation. Only applies to 2D Gaussians. Defaults to 0.
 
     Returns
@@ -131,8 +139,8 @@ def gaussian(amplitude, center, sigma, background, angle=0.):
                     background)
         return gaussian_1d
     elif ndim == 2:
-        cs = np.cos(angle)  # cache
-        sn = np.sin(angle)  # cache
+        cs = np.cos(rotation)  # cache
+        sn = np.sin(rotation)  # cache
 
         def rotate(x, y):
             return x*cs - y*sn, x*sn + y*cs
@@ -168,12 +176,14 @@ def fit(data, initial_guess=None):
     dict
         The dict contains the fitted parameters of the Gaussian. It dict has
         the follwing keys:
+
         - "amplitude" (float)
         - "center" (np.ndarray): coordinates of the center
         - "sigma" (np.ndarray)
         - "background" (float)
-        - "angle" (float): angle of rotation. Works (currently) only for 2D
+        - "rotation" (float): angle of rotation. Works (currently) only for 2D
           data.
+
         The keys match the arguments of `gaussian` so that the dict can be
         passed directly to the function.
 
@@ -191,7 +201,7 @@ def fit(data, initial_guess=None):
     def param_tuple_to_dict(params):
         return dict(amplitude=params[0], center=np.array(params[1:ndim+1]),
                     sigma=np.array(params[ndim+1:2*ndim+1]),
-                    background=params[2*ndim + 1], angle=params[2*ndim + 2])
+                    background=params[2*ndim + 1], rotation=params[2*ndim + 2])
 
     def err_func(params):
         params = param_tuple_to_dict(params)
@@ -205,9 +215,127 @@ def fit(data, initial_guess=None):
     initial_list = ([initial_guess["amplitude"]] +
                     initial_guess["center"].tolist() +
                     initial_guess["sigma"].tolist() +
-                    [initial_guess["background"], initial_guess["angle"]])
+                    [initial_guess["background"], initial_guess["rotation"]])
     p, success = scipy.optimize.leastsq(err_func, initial_list)
 
     if success > 2:
         raise RuntimeError("Fit did not converge.")
     return param_tuple_to_dict(p)
+
+
+def gaussian_1d(x, amplitude=1., center=0., sigma=1., background=0.):
+    r"""1D gaussian
+
+    .. math:: A e^\frac{(x - c)^2}{2\sigma^2} + b
+
+    Parameters
+    ----------
+    x : numpy.ndarray
+        Function arguments
+    amplitude : float, optional
+        `A` in the formula above. Defaults to 1.
+    center : float, optional
+        `c` in the formula above. Defaults to 0.
+    sigma : float, optional
+        :math:`\sigma` in the formula above. Defaults to 1.
+    background : float, optional
+        `b` in the formula above. Defaults to 0.
+
+    Returns
+    -------
+    numpy.ndarray
+        Function values
+    """
+    return amplitude * np.exp(-((x - center)/sigma)**2/2.) + background
+
+
+def gaussian_2d(x, y, amplitude=1., centerx=0., sigmax=1., centery=0.,
+                sigmay=1., background=0., rotation=0.):
+    r"""2D gaussian
+
+    .. math:: A \exp(\frac{(R(x - c_x))^2}{2\sigma_x^2}
+        + \frac{(R(y - c_y))^2}{2\sigma_y^2}) + b,
+
+    where :math:`R` rotates the vector (x, y) by `rotation` radiants.
+
+    Parameters
+    ----------
+    x, y : numpy.array
+        Function arguments
+    amplitude : float, optional
+        `A` in the formula above. Defaults to 1.
+    centerx, centery : float, optional
+        :math:`c_x`, :math:`c_y` in the formula above. Defaults to 0.
+    sigmax, sigmay : float, optional
+        :math:`\sigma_x`,  :math:`\sigma_y` in the formula above. Defaults
+        to 1.
+    background : float, optional
+        `b` in the formula above. Defaults to 0.
+    rotation : float, optional
+        Rotate the Gaussian by that many radiants. Defaults to 0
+
+    Returns
+    -------
+    numpy.ndarray
+        Function values
+    """
+    cs = np.cos(rotation)
+    sn = np.sin(rotation)
+
+    xc_r = centerx*cs - centery*sn  # rotate center coordinates
+    yc_r = centerx*sn + centery*cs
+
+    x_r = x*cs - y*sn  # rotate independent variable
+    y_r = x*sn + y*cs
+
+    arg = ((x_r - xc_r)/sigmax)**2 + ((y_r - yc_r)/sigmay)**2
+    return amplitude * np.exp(-arg/2.) + background
+
+
+class Gaussian1DModel(lmfit.Model):
+    """Model class for fitting a 1D Gaussian
+
+    Derives from :class:`lmfit.Model`
+
+    Parameters are `amplitude`, `center`, `sigma`, `background`.
+    """
+    def __init__(self, *args, **kwargs):
+        """Constructor""" + lmfit.models.COMMON_DOC
+        super().__init__(gaussian_1d, *args, **kwargs)
+        self.set_param_hint("sigma", min=0.)
+        self.set_param_hint("background", min=0.)
+
+    def guess(self, data, x, **kwargs):
+        pdict = guess_paramaters(data, x)
+        pars = self.make_params(amplitude=pdict["amplitude"],
+                                center=pdict["center"][0],
+                                sigma=pdict["sigma"][0],
+                                background=pdict["background"])
+        return lmfit.models.update_param_vals(pars, self.prefix, **kwargs)
+
+
+class Gaussian2DModel(lmfit.Model):
+    """Model class for fitting a 2D Gaussian
+
+    Derives from :class:`lmfit.Model`
+    """
+    def __init__(self, *args, **kwargs):
+        """Constructor""" + lmfit.models.COMMON_DOC
+        super().__init__(gaussian_2d, independent_vars=["x", "y"],
+                         *args, **kwargs)
+        self.set_param_hint("sigmax", min=0.)
+        self.set_param_hint("sigmay", min=0.)
+        self.set_param_hint("rotation", min=-np.pi, max=np.pi)
+        self.set_param_hint("background", min=0.)
+
+    def guess(self, data, x, y, **kwargs):
+        """Make an initial guess using :func:`guess_parameters`"""
+        pdict = guess_paramaters(data, x, y)
+        pars = self.make_params(amplitude=pdict["amplitude"],
+                                centerx=pdict["center"][0],
+                                sigmax=pdict["sigma"][0],
+                                centery=pdict["center"][1],
+                                sigmay=pdict["sigma"][1],
+                                background=pdict["background"],
+                                rotation=pdict["rotation"])
+        return lmfit.models.update_param_vals(pars, self.prefix, **kwargs)
--- tests/test_gaussian.py
+++ tests/test_gaussian.py
@@ -16,26 +16,27 @@ data_path = os.path.join(path, "data_gaussian")
 
 class TestGaussianFit(unittest.TestCase):
     def setUp(self):
-        self.params_1d = dict(amplitude=10, center=[40], sigma=[15],
-                              background=20, angle=0)
-        self.params_2d = dict(amplitude=10, center=[40, 120], sigma=[40, 15],
-                              background=15, angle=np.deg2rad(30))
+        self.params_1d = dict(amplitude=10, center=40, sigma=15,
+                              background=20)
+        self.x_1d = np.arange(100)
+        self.params_2d = dict(amplitude=10, centerx=40, centery=120,
+                              sigmax=40, sigmay=15, background=15,
+                              rotation=np.deg2rad(30))
+        self.x_2d = np.indices((100, 200))
 
     def test_gaussian_1d(self):
         orig = np.load(os.path.join(data_path, "gaussian_1d.npy"))
-        gauss = sdt.gaussian_fit.gaussian(**self.params_1d)
-        x = np.arange(100)
-        np.testing.assert_allclose(gauss(x), orig)
+        gauss = sdt.gaussian_fit.gaussian_1d(self.x_1d, **self.params_1d)
+        np.testing.assert_allclose(gauss, orig)
 
     def test_gaussian_2d(self):
         orig = np.load(os.path.join(data_path, "gaussian_2d.npy"))
-        gauss = sdt.gaussian_fit.gaussian(**self.params_2d)
-        i, j = np.indices((100, 200))
-        np.testing.assert_allclose(gauss(i, j), orig)
+        gauss = sdt.gaussian_fit.gaussian_2d(*self.x_2d, **self.params_2d)
+        np.testing.assert_allclose(gauss, orig)
 
     def test_guess_1d(self):
         data = np.load(os.path.join(data_path, "gaussian_1d.npy"))
-        guess = sdt.gaussian_fit.guess_paramaters(data)
+        guess = sdt.gaussian_fit.guess_paramaters(data, self.x_1d)
 
         for k, v in guess.items():
             # if the guess is within 10% of the actual values, this is good
@@ -43,31 +44,49 @@ class TestGaussianFit(unittest.TestCase):
 
     def test_guess_2d(self):
         data = np.load(os.path.join(data_path, "gaussian_2d.npy"))
-        guess = sdt.gaussian_fit.guess_paramaters(data)
+        guess = sdt.gaussian_fit.guess_paramaters(data, *self.x_2d)
 
         for k, v in guess.items():
-            if k == "angle":
-                # This can be way off
+            if k == "rotation":
+                # this can be way off
                 continue
-            if k == "sigma":
-                # also not very good
-                np.testing.assert_allclose(v, self.params_2d[k], rtol=1)
-                continue
-            np.testing.assert_allclose(v, self.params_2d[k], rtol=0.2)
+            if k == "center":
+                np.testing.assert_allclose(v[0],
+                                           self.params_2d["centerx"],
+                                           rtol=0.2)
+                np.testing.assert_allclose(v[1],
+                                           self.params_2d["centery"],
+                                           rtol=0.2)
+            elif k == "sigma":
+                # not very accurate due to rotation
+                np.testing.assert_allclose(v[0],
+                                           self.params_2d["sigmax"],
+                                           rtol=1)
+                np.testing.assert_allclose(v[1],
+                                           self.params_2d["sigmay"],
+                                           rtol=1)
+            else:
+                np.testing.assert_allclose(v, self.params_2d[k], rtol=0.2)
 
-    def test_fit_1d(self):
+    def test_lmfit_1d(self):
         data = np.load(os.path.join(data_path, "gaussian_1d.npy"))
-        fit = sdt.gaussian_fit.fit(data)
+        m = sdt.gaussian_fit.Gaussian1DModel()
+        g = m.guess(data, x=self.x_1d)
+        f = m.fit(data, params=g, x=self.x_1d)
 
-        for k, v in fit.items():
+        for k, v in f.best_values.items():
             np.testing.assert_allclose(v, self.params_1d[k], rtol=1e-5)
 
-    def test_fit_2d(self):
+    def test_lmfit_2d(self):
         data = np.load(os.path.join(data_path, "gaussian_2d.npy"))
-        fit = sdt.gaussian_fit.fit(data)
-
-        for k, v in fit.items():
-            np.testing.assert_allclose(v, self.params_2d[k], rtol=1e-5)
+        m = sdt.gaussian_fit.Gaussian2DModel()
+        g = m.guess(data, *self.x_2d)
+        f = m.fit(data, params=g, x=self.x_2d[0], y=self.x_2d[1])
+        for k, v in f.best_values.items():
+            if k == "rotation":
+                np.testing.assert_allclose(v, self.params_2d[k], rtol=1e-1)
+            else:
+                np.testing.assert_allclose(v, self.params_2d[k], rtol=1e-2)
 
 
 if __name__ == "__main__":
