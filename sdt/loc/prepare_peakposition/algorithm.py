728de37c338c64ae883eee35dd0f66aa6bbca9b1
Lukas Schrangl <lukas.schrangl@tuwien.ac.at>
2016-02-07 12:42:33 +0100

    prepare_peakposition: Make fitter margin handling more DAOSTORM-like
    Draw a margin around the image by reflecting the edges (10px) and set
    the fitter margin to 10. This increases the maximum fitting range per
    peak.

--- sdt/loc/prepare_peakposition/algorithm.py
+++ sdt/loc/prepare_peakposition/algorithm.py
@@ -1,10 +1,19 @@
+from ..daostorm_3d.algorithm import make_margin
+from ..daostorm_3d.data import col_nums
+
+
 def locate(raw_image, diameter, threshold, im_size, finder_class,
-           fitter_class):
+           fitter_class, fitter_margin=10):
     finder = finder_class(diameter, im_size)
     peaks = finder.find(raw_image, threshold)
 
-    fitter = fitter_class(raw_image, peaks, margin=finder.im_size)
-    fitter.fit()
-    peaks = fitter.peaks
+    img_with_margin = make_margin(raw_image, fitter_margin)
+    peaks[:, [col_nums.x, col_nums.y]] += fitter_margin
 
-    return fitter.peaks
+    fitter = fitter_class(img_with_margin, peaks, margin=fitter_margin)
+    fitter.fit()
+
+    peaks = fitter.peaks
+    peaks[:, [col_nums.x, col_nums.y]] -= fitter_margin
+
+    return peaks
