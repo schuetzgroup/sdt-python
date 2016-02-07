bbeff078a7a43fd8035d0979afca9b3fa2a0d66e
Lukas Schrangl <lukas.schrangl@tuwien.ac.at>
2016-02-07 13:37:33 +0100

    prepare_peakposition: Implement batch()
    This also adds a max_iterations parameter to locate().

--- sdt/loc/prepare_peakposition/algorithm.py
+++ sdt/loc/prepare_peakposition/algorithm.py
@@ -1,9 +1,10 @@
 def locate(raw_image, radius, threshold, im_size, finder_class,
-           fitter_class):
+           fitter_class, max_iterations):
     finder = finder_class(radius, im_size)
     peaks = finder.find(raw_image, threshold)
 
     fitter = fitter_class(raw_image, peaks, margin=finder.im_size)
+    fitter.max_iterations = max_iterations
     fitter.fit()
 
     return fitter.peaks
--- sdt/loc/prepare_peakposition/api.py
+++ sdt/loc/prepare_peakposition/api.py
@@ -20,7 +20,8 @@ except ImportError as e:
         "slow pure python fitter. Error message: {}.".format(str(e)))
 
 
-def locate(raw_image, radius, threshold, im_size=2, engine="numba"):
+def locate(raw_image, radius, threshold, im_size=2, engine="numba",
+           max_iterations=200):
     if engine == "numba" and numba_available:
         Finder = find_numba.Finder
         Fitter = fit_numba_impl.Fitter2D
@@ -31,7 +32,7 @@ def locate(raw_image, radius, threshold, im_size=2, engine="numba"):
         raise ValueError("Unknown engine: " + str(engine))
 
     peaks = algorithm.locate(raw_image, radius, threshold, im_size,
-                             Finder, Fitter)
+                             Finder, Fitter, max_iterations)
 
     # Create DataFrame
     converged_peaks = peaks[peaks[:, col_nums.stat] == feat_status.conv]
@@ -50,3 +51,51 @@ def locate(raw_image, radius, threshold, im_size=2, engine="numba"):
         df["frame"] = raw_image.frame_no
 
     return df
+
+
+def batch(frames, radius, threshold, im_size, engine="numba",
+          max_iterations=200):
+    """Call `locate` on a series of frames.
+
+    For details, see the `locate` documentation.
+
+    Parameters
+    ----------
+    frames : iterable of images
+        Iterable of array-like objects that represent image data
+    radius : float
+        This is in units of pixels. Initial guess for the radius of the
+        features.
+    threshold : float
+        Use a number roughly equal to the integrated intensity (mass) of the
+        dimmest peak (minus the CCD baseline) that should be detected. If this
+        is too low more background will be detected. If it is too high more
+        peaks will be missed.
+
+    Returns
+    -------
+    DataFrame([x, y, signal, bg, mass, size, frame])
+        x and y are the coordinates of the features. mass is the total
+        intensity of the feature, bg the background per pixel. size gives the
+        radii (sigma) of the featurs. frame is the frame number.
+
+    Other parameters
+    ----------------
+    engine : {"python", "numba"}, optional
+        Which engine to use for calculations. "numba" is much faster than
+        "python", but requires numba to be installed.
+    max_iterations : int, optional
+        Maximum number of iterations for peak fitting. Default: 200
+    """
+    all_features = []
+    for i, img in enumerate(frames):
+        features = locate(img, radius, threshold, im_size, engine,
+                          max_iterations)
+
+        if not hasattr(img, "frame_no") or img.frame_no is None:
+            features["frame"] = i
+            # otherwise it has been set in locate()
+
+        all_features.append(features)
+
+    return pd.concat(all_features, ignore_index=True)
