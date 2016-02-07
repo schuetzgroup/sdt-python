a4ad9292fc866408c5ef0b7f678d90f70d821215
Lukas Schrangl <lukas.schrangl@tuwien.ac.at>
2016-02-07 13:17:06 +0100

    Revert "prepare_peakposition: Make fitter margin handling more DAOSTORM-like"
    This reverts commit 728de37c338c64ae883eee35dd0f66aa6bbca9b1.
    While propably technically superior, this commit made parameters like mass
    and bg deviate very strongly from the MATLAB implementation.

--- sdt/loc/prepare_peakposition/algorithm.py
+++ sdt/loc/prepare_peakposition/algorithm.py
@@ -1,19 +1,9 @@
-from ..daostorm_3d.algorithm import make_margin
-from ..daostorm_3d.data import col_nums
-
-
 def locate(raw_image, diameter, threshold, im_size, finder_class,
-           fitter_class, fitter_margin=10):
+           fitter_class):
     finder = finder_class(diameter, im_size)
     peaks = finder.find(raw_image, threshold)
 
-    img_with_margin = make_margin(raw_image, fitter_margin)
-    peaks[:, [col_nums.x, col_nums.y]] += fitter_margin
-
-    fitter = fitter_class(img_with_margin, peaks, margin=fitter_margin)
+    fitter = fitter_class(raw_image, peaks, margin=finder.im_size)
     fitter.fit()
 
-    peaks = fitter.peaks
-    peaks[:, [col_nums.x, col_nums.y]] -= fitter_margin
-
-    return peaks
+    return fitter.peaks
