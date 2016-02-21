c2fde4c67b239a973ef88e80845c9cc00663b5cf
Lukas Schrangl <lukas.schrangl@tuwien.ac.at>
2016-02-21 17:15:41 +0100

    loc.cg: Make local maximum finder fully compatible
    Also, add tests for that.

--- sdt/loc/cg/find.py
+++ sdt/loc/cg/find.py
@@ -3,6 +3,11 @@ from scipy import ndimage
 
 
 def find(image, search_radius, threshold):
+    # reverse column order to convert image matrix indices to x, y coordinates
+    return local_maxima(image, search_radius, threshold)[:, ::-1]
+
+
+def local_maxima(image, search_radius, threshold):
     # create circular mask with radius `search_radius`
     x_sq = np.arange(-search_radius, search_radius + 1)**2
     mask = x_sq[:, np.newaxis] + x_sq[np.newaxis, :]
@@ -17,26 +22,35 @@ def find(image, search_radius, threshold):
 
     # wherever the image value is equal to the dilated value, we have a
     # candidate for a maximum
-    candidates = np.nonzero(np.isclose(dil, image) & (image > threshold))
+    candidates = np.nonzero(np.isclose(dil, image) & (image >= threshold))
     candidates = np.transpose(candidates)
 
     # discard maxima within `search_radius` pixels of the edges
     in_margin = np.any(
         (candidates < search_radius) |
-        (candidates > np.array(image.shape) - search_radius - 1), axis=1)
+        (candidates >= np.array(image.shape) - search_radius - 1), axis=1)
     candidates = candidates[~in_margin]
 
     # remove spurious maxima which result from flat peaks
-    is_max = np.empty(len(candidates), dtype=np.bool)
+    is_max = np.zeros(len(candidates), dtype=np.bool)
     candidate_list = candidates.T.tolist()
     max_img = np.zeros(image.shape)
     max_img[candidate_list] = image[candidate_list]
+
     for cnt, (i, j) in enumerate(candidates):
         roi = max_img[i-search_radius:i+search_radius+1,
-                      j-search_radius:j+search_radius+1]
-        roi *= mask
-        if np.sum(roi >= image[i, j]) == 1:
-            # this is the only maximum (left), keep it
+                      j-search_radius:j+search_radius+1] * mask
+
+        # complicated, but that's the way the original implementation does it
+        # and we want to be compatible
+        sort_val = np.sort(roi, axis=0)
+        sort_idx = np.argsort(roi, axis=0)
+        max_j = np.argmax(sort_val[-1, :])  # column of the max value
+        max_i = sort_idx[-1, max_j]         # row of the max value
+        if (max_i == search_radius) and (max_j == search_radius):
+            # the current value is the maximum in the ROI, since
+            # max_i == max_j == search_radius means that the max is in the
+            # center of the ROI
             is_max[cnt] = True
         else:
             # delete from max_img
