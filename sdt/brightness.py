6b51e7d935df7cf25ac290ab5513a49a5f3817d9
Lukas Schrangl <lukas.schrangl@tuwien.ac.at>
2016-03-04 12:57:34 +0100

    brightness: Overhaul
    - Convert to numpy docstring format
    - Use radii instead of diameters
    - Update DataFrame column names
    - Add tests

--- sdt/brightness.py
+++ sdt/brightness.py
@@ -1,130 +1,126 @@
-# -*- coding: utf-8 -*-
 """
-Find signal brightness
+Collection of functions related to the brightness of fluorophores
 
 Attributes:
     pos_colums (list of str): Names of the columns describing the x and the y
         coordinate of the features in pandas.DataFrames. Defaults to
         ["x", "y"].
-    t_column (str): Name of the column containing frame numbers. Defaults
-        to "frame".
-    mass_column (str): Name of the column describing the integrated intensities
-        ("masses") of the features. Defaults to "mass".
-    bg_column (str): Name of the column describing background per pixel.
-        Defaults to "background".
-    bg_dev_column (str): Name of the column describing background deviation per
-        pixel. Defaults to "bg_deviation".
 """
 import numpy as np
+import pandas as pd
+import scipy.stats
 
 
 pos_columns = ["x", "y"]
 t_column = "frame"
 mass_column = "mass"
 bg_column = "background"
-bg_dev_column = "bg_deviation"
+bg_dev_column = "bg_dev"
 
 
-def _get_raw_brightness_single(data, frames, diameter=5, bg_frame=1):
+def _from_raw_image_single(data, frames, radius=2, bg_frame=2):
     """Determine brightness by counting pixel values for a single particle
 
     This is called using numpy.apply_along_axis on the whole dataset.
 
-    Args:
-        data (list): First entry is the frame number, other entries are
-            particle coordinates.
-        frames (list of numpy.arrays): Raw image data
-        diameter (int): Length of the box in which pixel values are summed up.
-            Has to be an odd number.
-        bg_frame (int, optional): Width of frame (in pixels) for background
-            determination.
+    Parameters
+    ----------
+    data : tuple of numbers
+        First entry is the frame number, other entries are particle
+        coordinates.
+    frames : iterable of numpy.ndarrays
+        Raw image data
+    radius : int
+        Half width of the box in which pixel values are summed up. E. g.
+        using ``radius=3`` leads to the summation of pixels in a square of
+        2*3 + 1 = 7 pixels width.
+    bg_frame : int, optional
+        Width of frame (in pixels) around the box used for brightness
+        determination for background determination. Defaults to 2.
 
-    Returns:
-        list whose first entry is the brightness, second entry is the
-        background per pixel, third entry is the deviation of the background.
+    Returns
+    -------
+    mass : float
+        Total brightness (minus background)
+    bg : float
+        Background intensity per pixel
+    bg_std : float
+        Standard deviation of the background
     """
     frameno = int(data[0])
-    pos = np.round(data[1:]) #round to nearest pixel value
-    ndim = len(pos) #number of dimensions; this could does not only work in 2D
-    fr = frames[frameno] #current image
-    sz = np.floor(diameter/2.)
-    start = pos - sz - bg_frame
-    end = pos + sz + bg_frame + 1
+    pos = np.round(data[1:])  # round to nearest pixel value
+    ndim = len(pos)  # number of dimensions
+    fr = frames[frameno]  # current image
+    start = pos - radius - bg_frame
+    end = pos + radius + bg_frame + 1
 
-    #this gives the pixels of the signal box plus background frame
-    signal_region = fr[ [slice(s, e) for s, e in zip(reversed(start),
-                                                     reversed(end))] ]
+    # this gives the pixels of the signal box plus background frame
+    signal_region = fr[[slice(s, e) for s, e in zip(reversed(start),
+                                                    reversed(end))]]
 
     if (signal_region.shape != end - start).any():
-        #The signal was too close to the egde of the image, we could not read
-        #all the pixels we wanted
+        # The signal was too close to the egde of the image, we could not read
+        # all the pixels we wanted
         mass = np.NaN
         background_intensity = np.NaN
         background_std = np.NaN
     elif bg_frame == 0 or bg_frame is None:
-        #no background correction
+        # no background correction
         mass = signal_region.sum()
         background_intensity = 0
         background_std = 0
     else:
-        #signal region without background frame (i. e. only the actual signal)
+        # signal region without background frame (i. e. only the actual signal)
         signal_slice = [slice(bg_frame, -bg_frame)]*ndim
         uncorr_intensity = signal_region[signal_slice].sum()
-        #TODO: threshold uncorr intensity?
+        # TODO: threshold uncorr intensity?
 
-        #background correction: Only take frame pixels
+        # background correction: Only take frame pixels
         signal_region[signal_slice] = 0
         background_pixels = signal_region[signal_region.nonzero()]
         background_intensity = np.mean(background_pixels)
         background_std = np.std(background_pixels)
-        mass = uncorr_intensity - background_intensity * diameter**ndim
+        mass = uncorr_intensity - background_intensity * (2*radius + 1)**ndim
 
     return [mass, background_intensity, background_std]
 
-def get_raw_brightness(positions, frames, diameter, bg_frame=2,
-                       pos_columns=pos_columns,
-                       t_column=t_column, mass_column=mass_column,
-                       bg_column=bg_column, bg_dev_column=bg_dev_column):
+
+def from_raw_image(positions, frames, radius, bg_frame=2,
+                   pos_columns=pos_columns):
     """Determine particle brightness by counting pixel values
 
-    Around each localization, all brightness values in a  `diameter` times
-    `diameter` square are added up. Additionally, background is locally
-    determined by calculating the mean brightness in a frame of `bg_frame`
-    pixels width around this box. This background is subtracted from the
-    signal brightness.
+    Around each localization, all brightness values in a  2*`radius` + 1 times
+    square are added up. Additionally, background is locally determined by
+    calculating the mean brightness in a frame of `bg_frame` pixels width
+    around this box. This background is subtracted from the signal brightness.
 
-    Args:
-        positions (pandas.DataFrame): Localization data. Brightness,
-            background, and background deviation columns are added and/or
-            replaced directly in this object.
-        frames (list of numpy.arrays): Raw image data
-        diameter (int): Length of the box in which pixel values are summed up.
-            Has to be an odd number.
-        bg_frame (int, optional): Width of frame (in pixels) for background
-            determination. Defaults to 2.
-        pos_columns (list of str, optional): Sets the `pos_columns` attribute.
-                Defaults to the `pos_columns` attribute of the module.
-        t_column (str, optional): Name of the column containing frame numbers.
-            Defaults to the `frameno_column` of the module.
-        mass_column (str, optional): Name of the column describing the
-            integrated intensities ("masses") of the features. Defaults to the
-            `mass_column` attribute of the module.
-        bg_column (str, optional): Name of the column containing the background
-            intensity per pixel. Defaults to the `bg_column` attribute of the
-            module.
-        bg_dev_column (str, optional): Name of the column describing background
-            deviation per pixel. Defaults to the `bg_dev_column` attribute of
-            the module.
+    Parameters
+    ----------
+    positions : pandas.DataFrame
+        Localization data. Brightness, background, and background deviation
+        columns are added and/or replaced directly in this object.
+    frames : list of numpy.ndarrays
+        Raw image data
+    radius (int):
+        Half width of the box in which pixel values are summed up. E. g.
+        using ``radius=3`` leads to the summation of pixels in a square of
+        2*3 + 1 = 7 pixels width.
+    bg_frame (int, optional): Width of frame (in pixels) for background
+        determination. Defaults to 2.
+    pos_columns : list of str, optional
+        Names of the columns describing the x and the y coordinates of the
+        features in ``positions``. Defaults to the ``pos_columns`` attribute
+        of the module.
     """
-    #convert to numpy array for performance reasons
+    # convert to numpy array for performance reasons
     t_pos_matrix = positions[[t_column] + pos_columns].as_matrix()
-    brightness = np.apply_along_axis(_get_raw_brightness_single, 1,
+    brightness = np.apply_along_axis(_from_raw_image_single, 1,
                                      t_pos_matrix,
-                                     frames, diameter, bg_frame)
+                                     frames, radius, bg_frame)
 
-    positions[mass_column] = brightness[:,0]
-    positions[bg_column] = brightness[:,1]
-    positions[bg_dev_column] = brightness[:,2]
+    positions[mass_column] = brightness[:, 0]
+    positions[bg_column] = brightness[:, 1]
+    positions[bg_dev_column] = brightness[:, 2]
 
 
 def distribution(data, abscissa, smooth=2.):
--- tests/test_brightness.py
+++ tests/test_brightness.py
@@ -5,7 +5,7 @@ import os
 import numpy as np
 import pandas as pd
 
-from sdt.brightness import distribution
+import sdt.brightness
 
 
 path, f = os.path.split(os.path.abspath(__file__))
@@ -14,13 +14,60 @@ loc_path = os.path.join(path, "data_data")
 
 
 class TestBrightness(unittest.TestCase):
+    def setUp(self):
+        self.radius = 2
+        self.bg_frame = 1
+        self.pos1 = [30, 20]
+        self.pos2 = [15, 10]
+
+        sig1 = np.full((2*self.radius + 1,)*2, 10.)
+        sig2 = np.full((2*self.radius + 1,)*2, 15.)
+
+        bg_radius = self.radius + self.bg_frame
+        bg = np.full((2*bg_radius + 1,)*2, 3.)
+        bg[:bg_radius, :bg_radius] = 4.
+        bg[bg_radius:, bg_radius:] = 4.
+        self.bg = 3.5
+        self.bg_dev = 0.5
+
+        self.mass1 = sig1.sum() - self.bg*(2*self.radius + 1)**2
+        self.mass2 = sig2.sum() - self.bg*(2*self.radius + 1)**2
+
+        self.img = np.zeros((50, 50))
+        self.img[self.pos1[1]-bg_radius:self.pos1[1]+bg_radius+1,
+                 self.pos1[0]-bg_radius:self.pos1[0]+bg_radius+1] = bg
+        self.img[self.pos2[1]-bg_radius:self.pos2[1]+bg_radius+1,
+                 self.pos2[0]-bg_radius:self.pos2[0]+bg_radius+1] = bg
+        self.img[self.pos1[1]-self.radius:self.pos1[1]+self.radius+1,
+                 self.pos1[0]-self.radius:self.pos1[0]+self.radius+1] = sig1
+        self.img[self.pos2[1]-self.radius:self.pos2[1]+self.radius+1,
+                 self.pos2[0]-self.radius:self.pos2[0]+self.radius+1] = sig2
+
+    def test_from_raw_image_single(self):
+        res = sdt.brightness._from_raw_image_single(
+            [0] + self.pos1, [self.img], self.radius, self.bg_frame)
+        np.testing.assert_allclose(
+            np.array(res), np.array([self.mass1, self.bg, self.bg_dev]))
+
+    def test_from_raw_image(self):
+        data = np.array([self.pos1, self.pos2])
+        data = pd.DataFrame(data, columns=["x", "y"])
+        data["frame"] = 0
+        expected = data.copy()
+        expected["mass"] = np.array([self.mass1, self.mass2])
+        expected["bg"] = self.bg
+        expected["bg_dev"] = self.bg_dev
+        sdt.brightness.from_raw_image(data, [self.img], self.radius,
+                                      self.bg_frame)
+        np.testing.assert_allclose(data, expected)
+
     def test_distribution(self):
         # output of MATLAB plotpdf
         orig = np.load(os.path.join(data_path, "plot_pdf_xy.npz"))
         # from data_data/pMHC_AF647_200k_000_.pkc
         data = pd.read_hdf(os.path.join(data_path, "peak_data.h5"), "features")
 
-        x, y = distribution(data, 10000, 3)
+        x, y = sdt.brightness.distribution(data, 10000, 3)
         np.testing.assert_allclose(x, orig["x"])
         np.testing.assert_allclose(y, orig["y"])
 
