997ec55ed8af93e9970130bc43066fec7f4f5860
Lukas Schrangl <lukas.schrangl@tuwien.ac.at>
2016-04-06 17:50:03 +0200

    gui.locator: Introduce command line options
    Specify whether to show a preview and which files to load

--- sdt/gui/locator/app.py
+++ sdt/gui/locator/app.py
@@ -1,8 +1,13 @@
 import traceback
 
+from qtpy.QtCore import QCommandLineParser, QCommandLineOption
 from qtpy.QtWidgets import QApplication, QMessageBox
 
 from .main_window import MainWindow
+
+
+def _tr(string):
+    return QApplication.translate("app", string)
 
 
 def run(argv):
@@ -19,6 +24,20 @@ def run(argv):
         exit status of the application
     """
     app = QApplication(argv)
+    app.setApplicationName("locator")
+
+    cp = QCommandLineParser()
+    cp.setApplicationDescription(_tr("Locate fluorescent features in images"))
+    cp.addHelpOption()
+    previewOption = QCommandLineOption(
+        ["p", "preview"], _tr("Show preview"), "true/false")
+    cp.addOption(previewOption)
+    cp.addPositionalArgument("files", _tr("Files to open, optional"))
+    cp.process(app)
+
+    preview = cp.value(previewOption)
+    files = cp.positionalArguments()
+
     try:
         w = MainWindow()
     except Exception as e:
@@ -27,5 +46,13 @@ def run(argv):
             app.translate("main", "Startup error"),
             str(e) + "\n\n" + traceback.format_exc())
         return 1
+
     w.show()
+
+    for f in files:
+        w.open(f)
+
+    if preview:
+        w.showPreview = preview.lower() in ("t", "1", "true", "yes")
+
     return app.exec_()
