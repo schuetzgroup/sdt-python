bbf6ecd3cca32740cd7c52ee076812b46e3e15df
Lukas Schrangl <schrangl@iap.tuwien.ac.at>
2016-05-13 16:47:50 +0200

    loc.daostorm_3d: Add `z` model support to high level API
    i. e. locate, batch, and friends.

--- sdt/loc/daostorm_3d/api.py
+++ sdt/loc/daostorm_3d/api.py
@@ -14,6 +14,7 @@ from . import find
 from . import algorithm
 from .. import make_batch
 from .. import restrict_roi
+from .. import z_fit
 
 numba_available = False
 try:
@@ -29,7 +30,7 @@ except ImportError as e:
 num_threads = multiprocessing.cpu_count()
 
 
-def locate(raw_image, radius, model, threshold, engine="numba",
+def locate(raw_image, radius, model, threshold, z_params=None, engine="numba",
            max_iterations=20):
     """Locate bright, Gaussian-like features in an image
 
@@ -45,15 +46,22 @@ def locate(raw_image, radius, model, threshold, engine="numba",
     radius : float
         This is in units of pixels. Initial guess for the radius of the
         features.
-    model : {"2dfixed", "2d", "3d"}
+    model : {"2dfixed", "2d", "3d", "z"}
         "2dfixed" will do fixed sigma 2d Gaussian fitting. "2d" does variable
         sigma 2d Gaussian fitting. "3d" means that x, y sigma are independently
         variables (elliptical Gaussian, this is intened for determining the
-        z position from astigmatism).
+        z position from astigmatism). When using "z", the z position is
+        varied to fit the signal x and y sigmas.
     threshold : float
         A number roughly equal to the value of the brightest pixel (minus the
         CCD baseline) in the dimmest peak to be detected. Local maxima with
         brightest pixels below this threshold will be discarded.
+    z_params : z_fit.Parameters or str or pandas.DataFrame or None
+        Only necessary if the `model` is "z" (then it cannot be `None`),
+        otherwise it is ignored. A :py:class:`z_fit.Parameters` object can be
+        passed or a filename to load the parameters from or a
+        py:class:`pandas.DataFrame` with calibration data (z vs. size_x and
+        size_y).
 
     Returns
     -------
@@ -73,6 +81,14 @@ def locate(raw_image, radius, model, threshold, engine="numba",
         Maximum number of iterations for successive peak finding and fitting.
         Default: 20
     """
+    if model == "z":
+        if z_params is None:
+            raise ValueError("Need to specify `z_params`")
+        if isinstance(z_params, str):
+            z_params = z_fit.Parameters.load(z_params)
+        elif isinstance(z_params, pd.DataFrame):
+            z_params = z_fit.Parameters.calibrate(z_params)
+
     if engine == "numba" and numba_available:
         Finder = find_numba.Finder
         if model == "2dfixed":
@@ -81,6 +97,8 @@ def locate(raw_image, radius, model, threshold, engine="numba",
             Fitter = fit_numba_impl.Fitter2D
         elif model == "3d":
             Fitter = fit_numba_impl.Fitter3D
+        elif model == "z":
+            Fitter = fit_numba_impl.fitter_z_factory(z_params)
         else:
             raise ValueError("Unknown model: " + str(model))
     elif engine == "python":
@@ -91,6 +109,8 @@ def locate(raw_image, radius, model, threshold, engine="numba",
             Fitter = fit_impl.Fitter2D
         elif model == "3d":
             Fitter = fit_impl.Fitter3D
+        elif model == "z":
+            Fitter = fit_impl.fitter_z_factory(z_params)
         else:
             raise ValueError("Unknown model: " + str(model))
     else:
@@ -105,12 +125,14 @@ def locate(raw_image, radius, model, threshold, engine="numba",
     df = pd.DataFrame(converged_peaks[:, [col_nums.x, col_nums.y, col_nums.amp,
                                           col_nums.bg]],
                       columns=["x", "y", "signal", "bg"])
+    if model == "z":
+        df["z"] = converged_peaks[:, col_nums.z]
 
     # integral of the 2D Gaussian 2 * pi * amplitude * sigma_x * sigma_y
     df["mass"] = (2 * np.pi * np.prod(
         converged_peaks[:, [col_nums.wx, col_nums.wy, col_nums.amp]], axis=1))
 
-    if model in ("3d", "Z"):
+    if model in ("3d", "z"):
         df["size_x"] = converged_peaks[:, col_nums.wx]
         df["size_y"] = converged_peaks[:, col_nums.wy]
     else:
--- tests/daostorm_3d/test_api.py
+++ tests/daostorm_3d/test_api.py
@@ -4,12 +4,14 @@ import os
 import numpy as np
 import pandas as pd
 
+from sdt.loc import z_fit
 from sdt.loc.daostorm_3d import locate
 
 
 path, f = os.path.split(os.path.abspath(__file__))
 data_path = os.path.join(path, "data_api")
 img_path = os.path.join(path, "data_find")
+z_path = os.path.join(path, "data_fit")
 
 
 class TestApi(unittest.TestCase):
@@ -17,7 +19,30 @@ class TestApi(unittest.TestCase):
         orig = pd.read_hdf(os.path.join(data_path, "locate_2dfixed.h5"),
                            "peaks")
         frame = np.load(os.path.join(img_path, "bead_img.npz"))["img"]
-        peaks = locate(frame, 1., "2dfixed", 400., "numba", 20)
+        peaks = locate(frame, 1., "2dfixed", 400., engine="numba",
+                       max_iterations=20)
+        np.testing.assert_allclose(peaks, orig[peaks.columns.tolist()])
+
+    def test_locate_z_no_params(self):
+        with self.assertRaises(ValueError):
+            locate(None, 1, "z", 100)
+
+    def test_locate_z(self):
+        orig = pd.read_hdf(os.path.join(data_path, "locate_z.h5"),
+                           "peaks")
+        frame = np.load(os.path.join(z_path, "z_sim_img.npz"))["img"]
+        z_params = z_fit.Parameters.load(os.path.join(z_path, "z_params.yaml"))
+        peaks = locate(frame, 1., "z", 200., z_params, engine="numba",
+                       max_iterations=10)
+        np.testing.assert_allclose(peaks, orig[peaks.columns.tolist()])
+
+    def test_locate_z_param_load(self):
+        orig = pd.read_hdf(os.path.join(data_path, "locate_z.h5"),
+                           "peaks")
+        frame = np.load(os.path.join(z_path, "z_sim_img.npz"))["img"]
+        z_params = os.path.join(z_path, "z_params.yaml")
+        peaks = locate(frame, 1., "z", 200., z_params, engine="numba",
+                       max_iterations=10)
         np.testing.assert_allclose(peaks, orig[peaks.columns.tolist()])
 
 
