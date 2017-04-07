83402d035fabfe5137e452b0962c3a3641a5dc53
Lukas Schrangl <schrangl@iap.tuwien.ac.at>
2017-04-07 12:48:33 +0200

    Create `plot` module
    This will contain functions for plotting data. For now, there is
    `density_scatter` for creating scatter plots where markers are colored
    according to data point density.

---
+++ sdt/plot.py
@@ -0,0 +1,58 @@
+"""Useful functions for plotting data"""
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import scipy.stats
+import numpy as np
+
+try:
+    import bokeh
+    import bokeh.plotting
+    bookeh_available = True
+except ImportError:
+    bookeh_available = False
+
+
+def density_scatter(x, y, ax=None, cmap="viridis", **kwargs):
+    """Make a scatter plot with points colored according to density
+
+    Use a Gaussian kernel density estimate to calculate the density of
+    data points and color them accordingly.
+
+    Parameters
+    ----------
+    x, y : array_like, shape(n, )
+        Input data
+    ax : None or matplotlib.axes.Axes or bokeh.plotting.Figure, optional
+        Object to use for drawing. If `None`, use matplotlib's current axes
+        (:py:func:`gca`).
+    cmap : str or matplotlib.colors.Colormap, optional
+        Name of colormap or `Colormap` instance to be used for mapping
+        densities to colors. Defaults to "viridis".
+    **kwargs
+        Additional keyword arguments to be passed to `ax`'s `scatter` method.
+    """
+    kernel = scipy.stats.gaussian_kde([x, y])
+    dens = kernel(kernel.dataset)
+
+    # sort so that highest densities are the last (makes nicer plots)
+    sort_idx = np.argsort(dens)
+    dens = dens[sort_idx]
+    x = x[sort_idx]
+    y = y[sort_idx]
+
+    if ax is None:
+        ax = plt.gca()
+
+    if isinstance(ax, plt.Axes):
+        kwargs["c"] = dens
+        kwargs["cmap"] = cmap
+    elif bookeh_available and isinstance(ax, bokeh.plotting.Figure):
+        cmap = mpl.cm.get_cmap(cmap)
+        cols = cmap((dens - dens.min())/dens.max()) * 255
+        kwargs["color"] = [bokeh.colors.RGB(*c) for c in cols.astype(int)]
+    else:
+        raise ValueError("Unsupported type for `ax`. Can be `None`, a "
+                         "`matplotlib.axes.Axes` instance, or "
+                         "a `bokeh.plotting.Figure` instance.")
+
+    ax.scatter(x, y, **kwargs)
