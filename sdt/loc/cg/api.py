301db48dd2a9494730d4affd2129bd9dda6455d6
Lukas Schrangl <schrangl@iap.tuwien.ac.at>
2017-06-14 19:21:43 +0200

    Small doc fix

--- sdt/loc/cg/api.py
+++ sdt/loc/cg/api.py
@@ -14,13 +14,13 @@ def locate(raw_image, radius, signal_thresh, mass_thresh, bandpass=True,
            noise_radius=1):
     """Locate bright, Gaussian-like features in an image
 
-    This implements an algorithm proposed by Crocker & Grier [1]_ and is based
-    on the implementation by the Kilfoil group, see
+    This implements an algorithm proposed by Crocker & Grier [Crocker1996]_
+    and is based on the implementation by the Kilfoil group, see
     http://people.umass.edu/kilfoil/tools.php
 
-    ..[1] Crocker, J. C. & Grier, D. G.: "Methods of digital video microscopy
-        for colloidal studies", Journal of colloid and interface science,
-        Elsevier, 1996, 179, 298-310
+    .. [Crocker1996] Crocker, J. C. & Grier, D. G.: "Methods of digital video
+        microscopy for colloidal studies", Journal of colloid and interface
+        science, Elsevier, 1996, 179, 298-310
 
     Parameters
     ----------
