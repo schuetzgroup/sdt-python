d06b9bc839dfdfef612af0e2768c7c00c5b7474d
Lukas Schrangl <schrangl@iap.tuwien.ac.at>
2018-01-24 18:55:12 +0100

    Import change point detection algorithms
    from https://github.com/hildensia/bayesian_changepoint_detection
    
    This includes offline and online change point detection algorithms. I
    also added regression tests for future modifications.

---
+++ sdt/changepoint/online.py
@@ -0,0 +1,92 @@
+from __future__ import division
+import numpy as np
+from scipy import stats
+
+def online_changepoint_detection(data, hazard_func, observation_likelihood):
+    maxes = np.zeros(len(data) + 1)
+    
+    R = np.zeros((len(data) + 1, len(data) + 1))
+    R[0, 0] = 1
+    
+    for t, x in enumerate(data):
+        # Evaluate the predictive distribution for the new datum under each of
+        # the parameters.  This is the standard thing from Bayesian inference.
+        predprobs = observation_likelihood.pdf(x)
+        
+        # Evaluate the hazard function for this interval
+        H = hazard_func(np.array(range(t+1)))
+       
+        # Evaluate the growth probabilities - shift the probabilities down and to
+        # the right, scaled by the hazard function and the predictive
+        # probabilities.
+        R[1:t+2, t+1] = R[0:t+1, t] * predprobs * (1-H)
+        
+        # Evaluate the probability that there *was* a changepoint and we're
+        # accumulating the mass back down at r = 0.
+        R[0, t+1] = np.sum( R[0:t+1, t] * predprobs * H)
+        
+        # Renormalize the run length probabilities for improved numerical
+        # stability.
+        R[:, t+1] = R[:, t+1] / np.sum(R[:, t+1])
+        
+        # Update the parameter sets for each possible run length.
+        observation_likelihood.update_theta(x)
+    
+        maxes[t] = R[:, t].argmax()
+    return R, maxes
+
+
+def constant_hazard(lam, r):
+    return 1/lam * np.ones(r.shape)
+
+
+class StudentT:
+    def __init__(self, alpha, beta, kappa, mu):
+        self.alpha0 = self.alpha = np.array([alpha])
+        self.beta0 = self.beta = np.array([beta])
+        self.kappa0 = self.kappa = np.array([kappa])
+        self.mu0 = self.mu = np.array([mu])
+
+    def pdf(self, data):
+        return stats.t.pdf(x=data, 
+                           df=2*self.alpha,
+                           loc=self.mu,
+                           scale=np.sqrt(self.beta * (self.kappa+1) / (self.alpha *
+                               self.kappa)))
+
+    def update_theta(self, data):
+        muT0 = np.concatenate((self.mu0, (self.kappa * self.mu + data) / (self.kappa + 1)))
+        kappaT0 = np.concatenate((self.kappa0, self.kappa + 1.))
+        alphaT0 = np.concatenate((self.alpha0, self.alpha + 0.5))
+        betaT0 = np.concatenate((self.beta0, self.beta + (self.kappa * (data -
+            self.mu)**2) / (2. * (self.kappa + 1.))))
+            
+        self.mu = muT0
+        self.kappa = kappaT0
+        self.alpha = alphaT0
+        self.beta = betaT0
+
+
+# Imported from https://github.com/hildensia/bayesian_changepoint_detection
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2014 Johannes Kulick
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
---
+++ tests/test_changepoint.py
@@ -0,0 +1,194 @@
+import unittest
+import os
+
+import numpy as np
+import scipy
+
+from sdt.changepoint import offline, online
+
+
+path, f = os.path.split(os.path.abspath(__file__))
+data_path = os.path.join(path, "data_changepoint")
+
+
+class TestOfflineObs(unittest.TestCase):
+    def _test_univ(self, func, res):
+        a = np.arange(10000)
+        r = func(a, 10, 1000)
+        self.assertAlmostEqual(r, res)
+
+    def _test_multiv(self, func, res):
+        a = np.arange(10000).reshape((-1, 2))
+        r = func(a, 10, 1000)
+        self.assertAlmostEqual(r, res)
+
+    def test_gaussian_obs_univ(self):
+        """changepoint.offline.gaussian_obs_log_likelihood, univariate
+
+        This is a regression test against the output of the original
+        implementation.
+        """
+        self._test_univ(offline.gaussian_obs_log_likelihood,
+                        -7011.825860906335)
+
+    def test_gaussian_obs_multiv(self):
+        """changepoint.offline.gaussian_obs_log_likelihood, multivariate
+
+        This is a regression test against the output of the original
+        implementation.
+        """
+        self._test_multiv(offline.gaussian_obs_log_likelihood,
+                          -16386.465097707242)
+
+    def test_ifm_obs_univ(self):
+        """changepoint.offline.ifm_obs_log_likelihood, univariate
+
+        This is a regression test against the output of the original
+        implementation.
+        """
+        self._test_univ(offline.ifm_obs_log_likelihood,
+                        -7716.5452917994835)
+
+    def test_ifm_obs_multiv(self):
+        """changepoint.offline.ifm_obs_log_likelihood, multivariate
+
+        This is a regression test against the output of the original
+        implementation.
+        """
+        self._test_multiv(offline.ifm_obs_log_likelihood,
+                          -16808.615307987133)
+
+    def test_fullcov_obs_univ(self):
+        """changepoint.offline.fullconv_obs_log_likelihood, univariate
+
+        This is a regression test against the output of the original
+        implementation.
+        """
+        self._test_univ(offline.fullcov_obs_log_likelihood,
+                        -7716.5452917994835)
+
+    def test_fullcov_obs_multiv(self):
+        """changepoint.offline.fullconv_obs_log_likelihood, univariate
+
+        This is a regression test against the output of the original
+        implementation.
+        """
+        self._test_multiv(offline.fullcov_obs_log_likelihood,
+                          -13028.349084233618)
+
+
+class TestOfflinePriors(unittest.TestCase):
+    def test_const_prior(self):
+        """changepoint.offline.const_prior"""
+        self.assertAlmostEqual(offline.const_prior(4, 100), 0.01)
+
+    def test_geometric_prior(self):
+        """changepoint.offline.geometric_prior"""
+        self.assertAlmostEqual(offline.geometric_prior(4, 0.1),
+                               0.9**3 * 0.1)
+
+    def test_neg_binomial_prior(self):
+        """changepoint.offline.neg_binomial_prior"""
+        t = 4
+        k = 100
+        p = 0.1
+        self.assertAlmostEqual(offline.neg_binominal_prior(t, k, p),
+                               (scipy.misc.comb(t - k, k - 1) * p**k *
+                                (1 - p)**(t - k)))
+
+
+class TestOfflineDetection(unittest.TestCase):
+    def setUp(self):
+        self.rand_state = np.random.RandomState(0)
+        self.data = np.concatenate([self.rand_state.normal(100, 10, 30),
+                                    self.rand_state.normal(30, 5, 40),
+                                    self.rand_state.normal(50, 20, 20)])
+        self.data2 = np.concatenate([self.rand_state.normal(40, 10, 30),
+                                     self.rand_state.normal(200, 5, 40),
+                                     self.rand_state.normal(80, 20, 20)])
+
+    def test_offline_changepoint_gauss_univ(self):
+        """changepoint.offline.offline_changepoint_detection: Gauss, univariate
+
+        This is a regression test against the output of the original
+        implementation.
+        """
+        Q, P, Pcp = offline.offline_changepoint_detection(
+            self.data,
+            lambda t: offline.const_prior(t, len(self.data)+1),
+            offline.gaussian_obs_log_likelihood,
+            truncate=-20)
+        orig = np.load(os.path.join(data_path, "offline_gauss_univ.npz"))
+        np.testing.assert_allclose(Q, orig["Q"])
+        np.testing.assert_allclose(P, orig["P"])
+        np.testing.assert_allclose(Pcp, orig["Pcp"])
+
+    def test_offline_changepoint_full_multiv(self):
+        """changepoint.offline.offline_changepoint_detection: full cov., multiv
+
+        This is a regression test against the output of the original
+        implementation.
+        """
+        Q, P, Pcp = offline.offline_changepoint_detection(
+            np.column_stack([self.data, self.data2]),
+            lambda t: offline.const_prior(t, len(self.data)+1),
+            offline.fullcov_obs_log_likelihood,
+            truncate=-20)
+        orig = np.load(os.path.join(data_path, "offline_full_multiv.npz"))
+        np.testing.assert_allclose(Q, orig["Q"])
+        np.testing.assert_allclose(P, orig["P"])
+        np.testing.assert_allclose(Pcp, orig["Pcp"])
+
+
+class TestOnline(unittest.TestCase):
+    def setUp(self):
+        self.rand_state = np.random.RandomState(0)
+        self.data = np.concatenate([self.rand_state.normal(100, 10, 30),
+                                    self.rand_state.normal(30, 5, 40),
+                                    self.rand_state.normal(50, 20, 20)])
+        self.t = online.StudentT(0.1, 0.01, 1, 0)
+
+    def test_constant_hazard(self):
+        """changepoint.online.constant_hazard"""
+        lam = 2
+        a = np.arange(10).reshape((2, -1))
+        h = online.constant_hazard(lam, a)
+        np.testing.assert_allclose(h, 1/lam * np.ones_like(a))
+
+    def test_stundentt_updatetheta(self):
+        """changepoint.online.StudentT.update_theta
+
+        This is a regression test against the output of the original
+        implementation.
+        """
+        self.t.update_theta(self.data[0])
+        np.testing.assert_allclose(self.t.alpha, [0.1, 0.6])
+        np.testing.assert_allclose(self.t.beta,
+                                   [1.00000000e-02, 3.45983319e+03])
+        np.testing.assert_allclose(self.t.kappa, [1., 2.])
+        np.testing.assert_allclose(self.t.mu, [0., 58.82026173])
+
+    def test_stundentt_pdf(self):
+        """changepoint.online.StudentT.pdf
+
+        This is a regression test against the output of the original
+        implementation.
+        """
+        r = self.t.pdf(self.data[0])
+        np.testing.assert_allclose(r, [0.0002096872696608132])
+
+    def test_online_changepoint(self):
+        """changepoint.online.online_changepoint_detection
+
+        This is a regression test against the output of the original
+        implementation.
+        """
+        R, maxes = online.online_changepoint_detection(
+            self.data, lambda t: online.constant_hazard(250, t), self.t)
+        orig = np.load(os.path.join(data_path, "online.npz"))
+        np.testing.assert_allclose(R, orig["R"])
+        np.testing.assert_allclose(maxes, orig["maxes"])
+
+
+if __name__ == "__main__":
+    unittest.main()
