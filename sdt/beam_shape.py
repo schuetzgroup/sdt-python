615ae0d8c1baf72fabd73e71413015228c300374
Lukas Schrangl <schrangl@iap.tuwien.ac.at>
2018-03-08 14:17:26 +0100

    beam_shape.Corrector: Calculate beam shape also from single molecule data

--- sdt/beam_shape.py
+++ sdt/beam_shape.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """Correct for inhomogeneous illumination
 
 The intensity cross section of a laser beam is usually not flat, but a
@@ -18,6 +17,7 @@ mass_column : str
 import pandas as pd
 import numpy as np
 import scipy.interpolate as sp_int
+from scipy import stats
 
 import slicerator
 
@@ -25,25 +25,35 @@ from . import gaussian_fit as gfit
 
 
 pos_columns = ["x", "y"]
+mass_column = "mass"
 
 
 class Corrector(object):
     """Correct for inhomogeneous illumination
 
     This works by multiplying features' integrated intensities ("mass") by
-    a position-dependent correction factor calculated from a series of
-    images of a homogeneously fluorescent surface.
+    a position-dependent correction factor. The factor can be calculated from
+    a series of images of a fluorescent surface or from single molecule data.
 
-    The factor is calculated by
+    The factor is calculated in case of a fluorescent surface by
 
     - Taking the averages of the pixel intensities of the images
     - If requested, doing a 2D Gaussian fit
     - Normalizing, so that the maximum of the Gaussian or of the average image
       (if on Gaussian fit was performed) equals 1.0
 
+    or in the case of single molecule data by fitting a 2D Gaussian to their
+    "mass" values. Single molecule density fluctuations are take into account
+    by weighing data points inversely to their density in the fit.
+
     The "signal" and "mass" of a feature at position (x, y) are then divided by
     the value of the Gaussian at the positon (x, y) (or the pixel intensity of
-    the image) to yield a corrected value.
+    the image) to yield a corrected value. Also, image data can be corrected
+    analogously in a pixel-by-pixel fashion.
+
+    **Images (both for deterimnation of the correction and those to be
+    corrected) need to have their background subtracted for this to work
+    properly.**
 
     Attributes
     ----------
@@ -60,43 +70,92 @@ class Corrector(object):
         features.
     """
 
-    def __init__(self, *images, gaussian_fit=True, pos_columns=pos_columns):
+    def __init__(self, *data, gaussian_fit=True, shape=None,
+                 density_weight=True, pos_columns=pos_columns,
+                 mass_column=mass_column):
         """Parameters
         ----------
-        images : lists of numpy.ndarrays
-            List of images of a homogeneous surface
+        *data : iterables of image data or pandas.DataFrames
+            Collections of images fluorescent surfaces or single molecule
+            data to use for determining the correction function.
         gaussian_fit : bool, optional
-            Whether to fit a Gaussian to the averaged image. Default: True
+            Whether to fit a Gaussian to the averaged image. This is ignored
+            if `data` is single molecule data. Default: True
+        shape : tuple of int
+            If `data` is single molecule data and `shape` is not None, create
+            :py:attr:`avg_img` and :py:attr:`corr_img` from the fit using the
+            given shape. Defaults to None.
+        density_weight : bool, optional
+            If `data` is single molecule data, weigh data points inversely to
+            their density for fitting so that areas with higher densities don't
+            have a higher impact on the result. Defaults to True.
+
+        Other parameters
+        ----------------
+        pos_columns : list of str, optional
+            Names of the columns describing the x and the y coordinates of the
+            features in `positions`.
+        mass_column : str
+            Name of the column describing the total brightness (mass) of single
+            molecules. Only applicable if `data` are single molecule data.
+            Defaults to "mass".
         """
         self.pos_columns = pos_columns
 
-        # calculate the average profile image
-        self.avg_img = np.zeros(images[0][0].shape, dtype=np.float)
-        for stack in images:
-            for img in stack:
-                # divide by max so that intensity fluctuations don't affect
-                # the results
-                self.avg_img += img/img.max()
-        self.avg_img /= self.avg_img.max()
+        if isinstance(data[0], pd.DataFrame):
+            # Get the beam shape from single molecule brightness values
+            x = np.concatenate([d[pos_columns[0]].values for d in data])
+            y = np.concatenate([d[pos_columns[1]].values for d in data])
+            mass = np.concatenate([d[mass_column].values for d in data])
+            fin = np.isfinite(mass)
+            x = x[fin]
+            y = y[fin]
+            mass = mass[fin]
 
-        if gaussian_fit:
-            # do the fitting
+            if density_weight:
+                weights = stats.gaussian_kde([x, y])([x, y])
+                weights = weights.max() / weights
+            else:
+                weights = None
+
             m = gfit.Gaussian2DModel()
-            x, y = np.indices(self.avg_img.shape)
-            g = m.guess(self.avg_img, x, y)
-            g["offset"].set(min=0.)  # background cannot be less than 0.
-            self.fit_result = m.fit(self.avg_img, params=g, x=x, y=y)
-
-            # normalization factor so that the maximum of the Gaussian is 1.
-            gparm = self.fit_result.best_values
-            norm = gparm["amplitude"] + gparm["offset"]
-            # correction image
-            self.corr_img = self.fit_result.best_fit / norm
+            g = m.guess(mass, x, y)
+            g["offset"].set(value=0., vary=False)
+            self.fit_result = m.fit(mass, params=g, weights=weights, x=x, y=y)
+            if shape is None:
+                self.avg_img = None
+                self.corr_img = None
+            else:
+                y, x = np.indices(shape)
+                self.avg_img = self.fit_result.eval(x=x, y=y)
+                self.avg_img /= self.avg_img.max()
+                self.corr_img = self.avg_img
         else:
-            self.fit_result = None
-            self.corr_img = self.avg_img
-            self.interp = sp_int.RegularGridInterpolator(
-                [np.arange(i) for i in self.corr_img.shape], self.corr_img)
+            # calculate the average profile image
+            self.avg_img = np.zeros(data[0][0].shape, dtype=np.float)
+            for stack in data:
+                for img in stack:
+                    # divide by max so that intensity fluctuations don't affect
+                    # the results
+                    self.avg_img += img/img.max()
+            self.avg_img /= self.avg_img.max()
+
+            if gaussian_fit:
+                # do the fitting
+                m = gfit.Gaussian2DModel()
+                y, x = np.indices(self.avg_img.shape)
+                g = m.guess(self.avg_img, x, y)
+                g["offset"].set(value=0., vary=False)
+                self.fit_result = m.fit(self.avg_img, params=g, x=x, y=y)
+
+                # normalization factor so that the maximum of the Gaussian is 1
+                gparm = self.fit_result.best_values
+                self.corr_img = self.fit_result.best_fit / gparm["amplitude"]
+            else:
+                self.fit_result = None
+                self.corr_img = self.avg_img
+                self.interp = sp_int.RegularGridInterpolator(
+                    [np.arange(i) for i in self.corr_img.shape], self.corr_img)
 
     def __call__(self, data, inplace=False):
         """Do brightness correction on `features` intensities
@@ -134,7 +193,7 @@ class Corrector(object):
         else:
             @slicerator.pipeline
             def corr(img):
-                return img/self.corr_img
+                return img / self.corr_img
             return corr(data)
 
     def get_factors(self, x, y):
@@ -157,7 +216,6 @@ class Corrector(object):
         """
         if self.fit_result is not None:
             gparm = self.fit_result.best_values
-            norm = gparm["amplitude"] + gparm["offset"]
-            return 1. / (self.fit_result.eval(x=y, y=x)/norm)
+            return 1. / (self.fit_result.eval(x=x, y=y) / gparm["amplitude"])
         else:
             return 1. / self.interp(np.array([y, x]).T)
--- tests/test_beam_shape.py
+++ tests/test_beam_shape.py
@@ -14,41 +14,108 @@ data_path = os.path.join(path, "data_beam_shape")
 
 class TestBeamShape(unittest.TestCase):
     def setUp(self):
-        self.img_sz = 100
-        self.x = np.arange(self.img_sz)[np.newaxis, :]
-        self.y = np.arange(self.img_sz)[:, np.newaxis]
+        self.img_shape = (100, 150)
+        self.y, self.x = np.indices(self.img_shape)
         self.xc = 20
         self.yc = 50
         self.wx = 40
         self.wy = 20
+        self.amp = 2
 
-        argx = -(self.x-self.xc)**2 / (2*self.wx**2)
-        argy = -(self.y-self.yc)**2 / (2*self.wy**2)
-        self.img = np.exp(argx)*np.exp(argy)
+        self.img = self._make_gauss(self.x, self.y)
 
-    def test_init(self):
+    def _make_gauss(self, x, y):
+        argx = -(x - self.xc)**2 / (2 * self.wx**2)
+        argy = -(y - self.yc)**2 / (2 * self.wy**2)
+        return self.amp * np.exp(argx) * np.exp(argy)
+
+    def test_init_img_nofit(self):
+        """beam_shape.Corrector.__init__: Image data, no fit"""
         imgs = []
         for amp in range(3, 0, -1):
             img = amp * self.img
             imgs.append(img)
 
         corr = sdt.beam_shape.Corrector(imgs, gaussian_fit=False)
-        np.testing.assert_allclose(corr.avg_img, img)
+        np.testing.assert_allclose(corr.avg_img, self.img / self.img.max())
+        np.testing.assert_allclose(corr.corr_img, self.img / self.img.max())
+        self.assertFalse(corr.fit_result)
 
-    def test_fit(self):
-        corr = sdt.beam_shape.Corrector([self.img], gaussian_fit=True)
-        g2d = corr.fit_result.eval
-        np.testing.assert_allclose(g2d(x=self.y, y=self.x),
-                                   self.img, rtol=1e-5)
-        np.testing.assert_allclose(corr.fit_result.best_fit, self.img,
+    def _check_fit_result(self, res, expected):
+        self.assertEqual(res.keys(), expected.keys())
+        for k in expected:
+            i = res[k]
+            e = expected[k]
+            self.assertAlmostEqual(
+                i, e, 2,
+                msg="{} mismatch: expected {}, got {}".format(k, e, i))
+
+    def test_init_img_fit(self):
+        """beam_shape.Corrector.__init__: Image data, fit"""
+        imgs = []
+        for amp in range(3, 0, -1):
+            img = amp * self.img
+            imgs.append(img)
+
+        corr = sdt.beam_shape.Corrector(imgs, gaussian_fit=True)
+        np.testing.assert_allclose(corr.avg_img, self.img / self.img.max())
+        np.testing.assert_allclose(corr.corr_img, self.img / self.img.max())
+
+        expected = dict(amplitude=1, centerx=self.xc, sigmax=self.wx,
+                        centery=self.yc, sigmay=self.wy, offset=0, rotation=0)
+        self._check_fit_result(corr.fit_result.best_values, expected)
+
+    def test_init_list(self):
+        """beam_shape.Corrector.__init__: List of data points, not weighted"""
+        y, x = [i.flatten() for i in np.indices(self.img_shape)]
+        x = x[::10]
+        y = y[::10]
+        data = np.column_stack([x, y, self._make_gauss(x, y)])
+        df = pd.DataFrame(data, columns=["x", "y", "mass"])
+
+        corr = sdt.beam_shape.Corrector(df, density_weight=False,
+                                        shape=self.img_shape)
+
+        np.testing.assert_allclose(corr.avg_img, self.img / self.img.max(),
+                                   rtol=1e-5)
+        np.testing.assert_allclose(corr.corr_img, self.img / self.img.max(),
                                    rtol=1e-5)
 
+        expected = dict(amplitude=self.amp, centerx=self.xc, sigmax=self.wx,
+                        centery=self.yc, sigmay=self.wy, offset=0, rotation=0)
+        self._check_fit_result(corr.fit_result.best_values, expected)
+
+    def test_init_list_weighted(self):
+        """beam_shape.Corrector.__init__: List of data points, weighted"""
+        y, x = [i.flatten() for i in np.indices(self.img_shape)]
+        x = x[::10]
+        y = y[::10]
+        data = np.column_stack([x, y, self._make_gauss(x, y)])
+        df = pd.DataFrame(data, columns=["x", "y", "mass"])
+
+        corr = sdt.beam_shape.Corrector(df, density_weight=True,
+                                        shape=self.img_shape)
+
+        np.testing.assert_allclose(corr.avg_img, self.img / self.img.max(),
+                                   rtol=1e-5)
+        np.testing.assert_allclose(corr.corr_img, self.img / self.img.max(),
+                                   rtol=1e-5)
+
+        expected = dict(amplitude=self.amp, centerx=self.xc, sigmax=self.wx,
+                        centery=self.yc, sigmay=self.wy, offset=0, rotation=0)
+        self._check_fit_result(corr.fit_result.best_values, expected)
+
     def test_feature_correction(self):
-        xcoord = np.arange(self.img_sz)
-        ycoord = len(xcoord) - xcoord - 1
-        mass_orig = np.array([100]*len(xcoord))
-        mass = mass_orig * self.img[ycoord, xcoord]
-        pdata = pd.DataFrame(dict(x=xcoord, y=ycoord, mass=mass))
+        """beam_shape.Corrector.__call__: Single molecule data correction"""
+        x = np.concatenate(
+            [np.arange(self.img_shape[1]),
+             np.full(self.img_shape[0], self.img_shape[1] // 2)])
+        y = np.concatenate(
+            [np.full(self.img_shape[1], self.img_shape[0] // 2),
+             np.arange(self.img_shape[0])])
+        mass_orig = np.full(len(x), 100)
+        mass = mass_orig * self._make_gauss(x, y) / self.amp
+        pdata = pd.DataFrame(dict(x=x, y=y, mass=mass))
         pdata1 = pdata.copy()
 
         corr_img = sdt.beam_shape.Corrector([self.img], gaussian_fit=False)
@@ -61,13 +128,18 @@ class TestBeamShape(unittest.TestCase):
                                    rtol=1e-5)
 
     def test_image_correction_with_img(self):
+        """beam_shape.Corrector.__call__: Image correction, no fit"""
         corr_img = sdt.beam_shape.Corrector([self.img], gaussian_fit=False)
-        np.testing.assert_allclose(corr_img(self.img), np.ones(self.img.shape))
+        np.testing.assert_allclose(corr_img(self.img),
+                                   np.full(self.img.shape, self.amp))
 
     def test_image_correction_with_gauss(self):
+        """beam_shape.Corrector.__call__: Image correction, fit"""
         corr_g = sdt.beam_shape.Corrector([self.img], gaussian_fit=True)
-        np.testing.assert_allclose(corr_g(self.img), np.ones(self.img.shape),
+        np.testing.assert_allclose(corr_g(self.img),
+                                   np.full(self.img.shape, 2),
                                    rtol=1e-5)
+
 
 if __name__ == "__main__":
     unittest.main()
