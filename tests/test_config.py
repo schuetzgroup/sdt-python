a3917a5b065cb806217209fcd1e07544078fa98e
Lukas Schrangl <schrangl@iap.tuwien.ac.at>
2018-04-13 02:03:29 +0200

    config.use_defaults: Add test for decorating a method

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,17 +5,29 @@ from sdt import config
 
 class TestUseDefaults(unittest.TestCase):
     def setUp(self):
-        @config.use_defaults
-        def f(pos_columns=None):
-            return pos_columns
-        self.func = f
         self.pos_columns = ["x", "y"]
         config.rc["pos_columns"] = self.pos_columns
 
-    def test_decorator(self):
-        """config.use_defaults"""
-        self.assertEqual(self.func(), self.pos_columns)
-        self.assertEqual(self.func(None), self.pos_columns)
-        self.assertEqual(self.func(["z"]), ["z"])
+    def test_function_decorator(self):
+        """config.use_defaults: function decorator"""
+        @config.use_defaults
+        def f(pos_columns=None):
+            return pos_columns
+
+        self.assertEqual(f(), self.pos_columns)
+        self.assertEqual(f(None), self.pos_columns)
+        self.assertEqual(f(["z"]), ["z"])
         config.rc["pos_columns"] = ["x", "y", "z"]
-        self.assertEqual(self.func(), ["x", "y", "z"])
+        self.assertEqual(f(), ["x", "y", "z"])
+
+    def test_method_decorator(self):
+        """config.use_defaults: method decorator"""
+        class A:
+            @config.use_defaults
+            def __init__(self, pos_columns=None):
+                self.pos_columns = pos_columns
+        self.assertEqual(A().pos_columns, self.pos_columns)
+        self.assertEqual(A(None).pos_columns, self.pos_columns)
+        self.assertEqual(A(["z"]).pos_columns, ["z"])
+        config.rc["pos_columns"] = ["x", "y", "z"]
+        self.assertEqual(A().pos_columns, ["x", "y", "z"])
