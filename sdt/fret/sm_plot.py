1d3d578af99c132482826a59434351b34b29409c
Lukas Schrangl <schrangl@iap.tuwien.ac.at>
2018-04-15 11:55:51 +0200

    Add `fret.smfret_draw_track` function

--- sdt/fret/sm_plot.py
+++ sdt/fret/sm_plot.py
@@ -2,9 +2,10 @@ import math
 
 import numpy as np
 import pandas as pd
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-from ..plot import density_scatter
+from .. import plot, config
 
 
 def smfret_scatter(track_data, xdata=("fret", "eff"), ydata=("fret", "stoi"),
@@ -24,7 +25,7 @@ def smfret_scatter(track_data, xdata=("fret", "eff"), ydata=("fret", "stoi"),
         x = x[m]
         y = y[m]
         try:
-            density_scatter(x, y, ax=a, **scatter_args)
+            plot.density_scatter(x, y, ax=a, **scatter_args)
         except Exception:
             a.scatter(x, y, **scatter_args)
         a.set_title(k)
@@ -81,3 +82,86 @@ def smfret_hist(track_data, data=("fret", "eff"), frame=None, columns=2,
 
     fig.tight_layout()
     return fig, ax
+
+
+@config.use_defaults
+def smfret_draw_track(tracks, track_no, donor_img, acceptor_img, size,
+                      columns=8, figure=None, pos_colums=None):
+    """Draw donor and acceptor images for a track
+
+    For each frame in a track, draw the raw image in the proximity of the
+    feature localization.
+
+    Note: This is rather slow.
+
+    Parameters
+    ----------
+    tracks : pandas.DataFrame
+        smFRET tracking data as e.g. produced by :py:class:`SmFretTracker`
+    track_no : int
+        Track/particle number
+    donor_img, acceptor_img : list-like of numpy.ndarray
+        Image sequences of the donor and acceptor channels
+    size : int
+        For each feature, draw a square of ``2 * size + 1`` size pixels.
+    columns : int, optional
+        Arrange images in that many columns. Defaults to 8.
+    figure : matplotlib.figure.Figure or None, optional
+        Use this figure to draw. If `None`, use
+        :py:func:`matplotlib.pyplot.gcf`. Defaults to `None`.
+
+    Returns
+    -------
+    matplotlib.figure.Figure
+        The figure object used for plotting
+
+    Other parameters
+    ----------------
+    pos_columns : list of str or None
+        Names of the columns describing the coordinates of the features in
+        :py:class:`pandas.DataFrames`. If `None`, use the defaults from
+        :py:mod:`config`. Defaults to `None`.
+    """
+    if figure is None:
+        figure = plt.gcf()
+
+    trc = tracks[tracks["fret", "particle"] == track_no]
+    don_px = plot.get_raw_features(trc["donor"], donor_img, size, pos_colums)
+    acc_px = plot.get_raw_features(trc["acceptor"], acceptor_img, size,
+                                   pos_colums)
+
+    rows = int(np.ceil(len(trc)/columns))
+    gs = mpl.gridspec.GridSpec(rows*3, columns+1, wspace=0.1, hspace=0.1)
+
+    for i, idx in enumerate(trc.index):
+        f = int(trc.loc[idx, ("donor", "frame")])
+        px_d = don_px[idx]
+        px_a = acc_px[idx]
+
+        r = (i // columns) * 3
+        c = (i % columns) + 1
+
+        fno_ax = figure.add_subplot(gs[r, c])
+        fno_ax.text(0.5, 0., str(f), va="bottom", ha="center")
+        fno_ax.axis("off")
+
+        don_ax = figure.add_subplot(gs[r+1, c])
+        don_ax.imshow(px_d, cmap="gray", interpolation="none")
+        don_ax.axis("off")
+
+        acc_ax = figure.add_subplot(gs[r+2, c])
+        acc_ax.imshow(px_a, cmap="gray", interpolation="none")
+        acc_ax.axis("off")
+
+    for r in range(rows):
+        f_ax = figure.add_subplot(gs[3*r, 0])
+        f_ax.text(0, 0., "frame", va="bottom", ha="left")
+        f_ax.axis("off")
+        d_ax = figure.add_subplot(gs[3*r+1, 0])
+        d_ax.text(0, 0.5, "donor", va="center", ha="left")
+        d_ax.axis("off")
+        a_ax = figure.add_subplot(gs[3*r+2, 0])
+        a_ax.text(0, 0.5, "acceptor", va="center", ha="left")
+        a_ax.axis("off")
+
+    return figure
