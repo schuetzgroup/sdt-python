fe5c694b813937914df4801703db8dcf53331556
Lukas Schrangl <schrangl@iap.tuwien.ac.at>
2018-06-21 10:38:52 +0200

    loc.get_raw_features: Use `config.set_columns`

--- sdt/loc/raw_features.py
+++ sdt/loc/raw_features.py
@@ -2,8 +2,9 @@ import numpy as np
 
 from .. import config, helper
 
-@config.use_defaults
-def get_raw_features(loc_data, img_data, size, pos_columns=None):
+
+@config.set_columns
+def get_raw_features(loc_data, img_data, size, columns={}):
     """Get raw image data surrounding localizations
 
     For each localization, return the raw image data in the proximity
@@ -27,27 +28,30 @@ def get_raw_features(loc_data, img_data, size, pos_columns=None):
 
     Other parameters
     ----------------
-    pos_columns : list of str or None, optional
-        Names of the columns describing the coordinates of the features in
-        :py:class:`pandas.DataFrames`. If `None`, use the defaults from
-        :py:mod:`config`. Defaults to `None`.
+    columns : dict, optional
+        Override default column names as defined in :py:attr:`config.columns`.
+        Relevant names are `coords` and `time`.
+        This means, if your DataFrame has coordinate columns "x" and "z" and
+        the time column "alt_frame", set ``columns={"coords": ["x", "z"],
+        "time": "alt_frame"}``.
     """
     ret = {}
 
     # Sort by frame; this avoids re-reading images if they are e.g. pims
     # image sequences.
-    for f, arr in helper.split_dataframe(loc_data, "frame", pos_columns,
-                                         keep_index=True):
+    for f, arr in helper.split_dataframe(loc_data, columns["time"],
+                                         columns["coords"], keep_index=True):
         f = int(f)
         img = img_data[f]
         for a in arr:
-            i = a[:-len(pos_columns)]
+            i = a[:-len(columns["coords"])]
             if len(i) > 1:
                 i = tuple(i)
             else:
                 i = i[0]
 
-            pos = np.round(a[-len(pos_columns):].astype(float)).astype(int)
+            pos = np.round(a[-len(columns["coords"]):].astype(float))
+            pos = pos.astype(int)
             sl = [slice(p - size, p + size + 1) for p in pos[::-1]]
             ret[i] = img[sl]
     return ret
