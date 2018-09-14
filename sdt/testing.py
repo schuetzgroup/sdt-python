8758851a06cdb223a5ba73cabbd3440bbf43a105
Lukas Schrangl <schrangl@iap.tuwien.ac.at>
2018-09-14 13:15:03 +0200

    Add testing.dist_sample function

---
+++ sdt/testing.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+def dist_sample(dist_func, range, nsamp, anchors=100, func_type="pdf"):
+    """Create a sample whose distribution function is `dist_func`
+
+    **This is not a random sample.** Instead, the sample's distribution
+    function is accurately and smoothly given by `dist_func`. This can be used
+    for testing purposes.
+
+    Parameters
+    ----------
+    dist_func : callable
+        PDF or CDF of the sample. See alse the `func_type` param.
+    range : tuple of float
+        Sample value range
+    nsamp : int
+        Sample size
+    anchors : int, optional
+        The `range` will be divided into `anchors` parts; between those, the
+        returned sample's CDF will be linear (PDF will be constant).
+        `nsamp` should be larger than `anchors` by at least an order of
+        magnitude (more to get more precise results). Defaults to 100.
+    func_type : {"pdf", "cdf"}, optional
+        Whether `dist_func` is a PDF or a CDF. Defaults to "pdf".
+    """
+    obs = np.linspace(*range, anchors + 1)
+
+    if func_type.lower() == "pdf":
+        dx = (range[1] - range[0]) / (anchors + 1)
+        pdf = dist_func(obs) * dx
+        pdf = (pdf[:-1] + pdf[1:]) / 2
+    elif func_type.lower() == "cdf":
+        pdf = np.diff(dist_func(obs))
+
+    ret = []
+    for x1, x2, p in zip(obs[:-1], obs[1:], pdf):
+        ret.append(np.linspace(x1, x2, int(round(p * nsamp))))
+
+    return np.concatenate(ret)
---
+++ tests/test_testing.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+from sdt import testing
+
+
+d = (0.5, 2)
+f = 2 / 3
+tl = 0.01
+ran = (0, 0.5)
+
+
+def cdf(x):
+    return (1 - f * np.exp(-x / (4 * d[0] * tl)) -
+            (1 - f) * np.exp(-x / (4 * d[1] * tl)))
+
+
+def pdf(x):
+    return (f / (4 * d[0] * tl) * np.exp(-x / (4 * d[0] * tl)) +
+            (1 - f ) / (4 * d[1] * tl) * np.exp(-x / (4 * d[1] * tl)))
+
+
+def test_dist_sample_pdf():
+    """testing.dist_sample: PDF arg"""
+    samp = testing.dist_sample(pdf, ran, 10000, 200)
+    x = np.sort(samp)
+    y = np.linspace(0, 1, len(x), endpoint=False)
+
+    np.testing.assert_allclose(y, cdf(x), atol=1e-3, rtol=1e-2)
+
+
+def test_dist_sample_cdf():
+    """testing.dist_sample: CDF arg"""
+    samp = testing.dist_sample(cdf, ran, 10000, 200, func_type="cdf")
+    x = np.sort(samp)
+    y = np.linspace(0, 1, len(x), endpoint=False)
+
+    np.testing.assert_allclose(y, cdf(x), atol=1e-3, rtol=1e-2)
