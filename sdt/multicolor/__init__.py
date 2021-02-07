689ea3e25a9d498020774cc28fc0da1536b58a9d
Lukas Schrangl <schrangl@iap.tuwien.ac.at>
2021-02-07 13:22:03 +0100

    Turn multicolor into a sub-package
    In the future, FrameSelector and Registrator will be moved here as well.

---
+++ sdt/multicolor/__init__.py
@@ -0,0 +1,77 @@
+# SPDX-FileCopyrightText: 2021 Lukas Schrangl <lukas.schrangl@tuwien.ac.at>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Multi-color data analysis
+=========================
+
+This module provides functions to analyze multi-color single molecule data.
+In particular, there is support for
+
+- finding colocalizations
+- detecting and plotting codiffusion
+- merging data from different channels.
+
+Single molecule data have to be aligned first using e.g.
+:py:class:`sdt.chromatic.Corrector`.
+
+
+Examples
+--------
+
+To demonstrate how to find colocalizations, create fake data first:
+
+>>> loc1 = pandas.DataFrame([[1, 1], [11, 11]], columns=["x", "y"])
+>>> loc2 = pandas.DataFrame([[1, 2], [22, 22]], columns=["x", "y"])
+>>> loc1["frame"] = loc2["frame"] = 0
+
+Now, find the colocalizations using :py:func:`find_colocalizations`:
+
+>>> coloc = find_colocalizations(loc1, loc2, max_dist=2)
+>>> coloc
+  channel1          channel2
+         x  y frame        x  y frame
+0        1  1     0        1  2     0
+
+Detecting codiffusing particles works similar using
+:py:func:`find_codiffusion`:
+
+>>> trc1 = sdt.io.load("tracking1.h5")  # load data
+>>> trc2 = sdt.io.load("tracking2.h5")
+>>> codiff = find_codiffusion(trc1, trc2)
+
+A single codiffusing pair can be plot with help of :py:func:`plot_codiffusion`
+using the result of :py:func:`find_codiffusion`:
+
+>>> plot_codiffusion(codiff, particle=0)
+
+Data from two color channels can be merged so that colocalizing features only
+appear once using :py:func:`merge_channels`:
+
+>>> merged = merge_channels(loc1, loc2, max_dist=2)
+>>> merged
+    x   y  frame
+0   1   1      0
+1  11  11      0
+2  22  22      0
+
+
+Programming reference
+---------------------
+
+.. autofunction:: find_colocalizations
+.. autofunction:: calc_pair_distance
+.. autofunction:: find_codiffusion
+.. autofunction:: plot_codiffusion
+.. autofunction:: merge_channels
+.. autofunction:: find_closest_pairs
+
+
+Low level helper functions
+--------------------------
+
+.. autofunction:: find_closest_pairs
+"""
+from .coloc import (calc_pair_distance, find_closest_pairs,
+                    find_colocalizations, find_codiffusion, merge_channels,
+                    plot_codiffusion)
