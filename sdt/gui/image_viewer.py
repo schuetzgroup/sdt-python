41d5fa9543c89ad94fdc3ae5daccfb0aeba3eed7
Lukas Schrangl <schrangl@iap.tuwien.ac.at>
2021-02-15 19:32:42 +0100

    gui.ImageViewer: Use qml_wrapper.Window in __main__

--- sdt/gui/image_viewer.py
+++ sdt/gui/image_viewer.py
@@ -6,54 +6,10 @@ import sys
 
 from PyQt5 import QtCore, QtWidgets
 
-from .qml_wrapper import Component, Window
-
-
-main_qml = """
-import QtQuick 2.12
-import QtQuick.Controls 2.12
-import QtQuick.Layouts 1.12
-import Qt.labs.settings 1.0
-import SdtGui 1.0
-
-
-ApplicationWindow {
-    id: win
-    visible: true
-    width: 800
-    height: 600
-    property alias dataset: imView.dataset
-    ImageViewer {
-        id: imView
-        anchors.fill: parent
-    }
-    Settings {
-        id: settings
-        category: "Window"
-        property int width: 640
-        property int height: 400
-    }
-    Component.onCompleted: {
-        width = settings.width
-        height = settings.height
-    }
-    onClosing: {
-        settings.setValue("width", width)
-        settings.setValue("height", height)
-    }
-}
-"""
+from .qml_wrapper import Window
 
 
 if __name__ == "__main__":
-    def statusHandler(status):
-        if status == Component.Status.Error:
-            QtCore.QCoreApplication.exit(1)
-            return
-        if status == Component.Status.Ready:
-            comp.dataset = args.files
-            return
-
     app = QtWidgets.QApplication(sys.argv)
     app.setOrganizationName("schuetzgroup")
     app.setOrganizationDomain("biophysics.iap.tuwien.ac.at")
@@ -65,8 +21,9 @@ if __name__ == "__main__":
     ap.add_argument("files", help="image sequences to open", nargs="*")
     args = ap.parse_args()
 
-    comp = Component(main_qml)
-    comp.status_Changed.connect(statusHandler)
-    QtCore.QTimer.singleShot(0, lambda: statusHandler(comp.status_))
+    win = Window("ImageViewer")
+    if win.status_ == Window.Status.Error:
+        sys.exit(1)
+    win.dataset = args.files
 
-    app.exec_()
+    sys.exit(app.exec_())
