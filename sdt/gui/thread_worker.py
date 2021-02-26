783bfa0043e315539d3ef44f1e005df717943c69
Lukas Schrangl <schrangl@iap.tuwien.ac.at>
2021-02-27 00:45:22 +0100

    gui.ThreadWorker: Make interruption via exceptions safe
    Only raise _InterruptThread when executing the payload function. In
    particular, don't raise _InterruptThread when dealing with other (real)
    exceptions.

--- sdt/gui/thread_worker.py
+++ sdt/gui/thread_worker.py
@@ -10,11 +10,6 @@ from PyQt5 import QtCore
 from .. import helper
 
 
-class _StopThread(Exception):
-    """Raise this in worker thread to terminate it"""
-    pass
-
-
 class _InterruptThread(Exception):
     """Raise this in worker thread to stop current function call"""
     pass
@@ -36,6 +31,9 @@ class ThreadWorker(QtCore.QObject):
         self._args = ()
         self._kwargs = {}
         self._callCondition = threading.Condition()
+        self._exceptionLock = threading.Lock()
+        self._stopRequested = False
+        self._allowException = False
         self._workerThread = None
         self._busy = False
         self.enabled = True
@@ -46,8 +44,10 @@ class ThreadWorker(QtCore.QObject):
         Note that it is not possible to interrupt the execution of C code.
         Interruption will take place once execution returns to Python.
         """
-        if self.busy:
-            helper.raise_in_thread(self._workerThread.ident, _InterruptThread)
+        with self._exceptionLock:
+            if self._allowException:
+                helper.raise_in_thread(self._workerThread.ident,
+                                       _InterruptThread)
 
     def __call__(self, *args: Any, **kwargs: Any):
         """Execute function call
@@ -111,12 +111,16 @@ class ThreadWorker(QtCore.QObject):
             self._workerThread = threading.Thread(target=self._workerFunc)
             self._workerThread.start()
         else:
-            with self._callCondition:
-                self._args = None
-                self._kwargs = None
-                helper.raise_in_thread(self._workerThread.ident, _StopThread)
-                # There will be no _StopThread exception while thread is
-                # waiting for _callCondition, thus notify
+            with self._callCondition, self._exceptionLock:
+                self._args = ()
+                self._kwargs = {}
+                if self._allowException:
+                    # Currently executing `self._func`, which we can try to
+                    # interrupt by raising an exception.
+                    helper.raise_in_thread(self._workerThread.ident,
+                                           _InterruptThread)
+                self._stopRequested = True
+                # May be necessary to wake up
                 self._callCondition.notify()
             self._workerThread = None
         self.enabledChanged.emit(e)
@@ -124,10 +128,15 @@ class ThreadWorker(QtCore.QObject):
     def _workerFunc(self):
         """Actual function to be executed in worker process
 
-        Waits on ``self._callEvent``, calls ``self._func(*self._args,
-        **self._kwargs)``. Raising ``_StopThread`` will terminate the thread.
-        Raising ``_InterruptThread`` will abort the current call, but keep
-        the thread alive.
+        Waits on `self._callCondition`, calls ``self._func(*self._args,
+        **self._kwargs)``. Raising `_InterruptThread` will abort the current
+        call; make sure that this is save by checking `self._allowException`
+        while holding `self._exceptionLock`.
+
+        Right before calling `self._func`, `self._stopRequested` is
+        checked. If `True`, the thread is stopped. To speed up stopping, it
+        may be beneficial to raise `_InterruptThread` (if allowed, see
+        above) and notify `self._callCondition`.
 
         If the call completes successfully, :py:attr:`finished` is emitted
         with the result. On an exception, :py:attr:`error` is emitted with
@@ -136,26 +145,44 @@ class ThreadWorker(QtCore.QObject):
         while True:
             try:
                 with self._callCondition:
-                    if self._args is None or self._kwargs is None:
+                    if None in (self._args, self._kwargs):
+                        # Idle, wait for a new call
+                        self._busy = False
+                        self.busyChanged.emit()
                         self._callCondition.wait()
                     args = self._args
                     kwargs = self._kwargs
+                    # Reset to `None`. If still `None` in the next loop run
+                    # (i.e., they have not been set anew by  `__call__`), the
+                    # worker will go to sleep.
                     self._args = None
                     self._kwargs = None
-                result = self._func(*args, **kwargs)
-                # FIXME: Make sure that _StopThread and _InterruptThread
-                # are not raised while executing code below. Otherwise there
-                # will be an "exception during handling exception" error.
-            except _StopThread:
-                return
+                try:
+                    with self._exceptionLock:
+                        # Check if thread should exit
+                        if self._stopRequested:
+                            # Don't forget to reset
+                            self._stopRequested = False
+                            return
+                        # Tell `enabled` setter and `abort` that they can
+                        # raise an exception to interrupt thread execution
+                        self._allowException = True
+                    # Actual function call
+                    result = self._func(*args, **kwargs)
+                finally:
+                    with self._exceptionLock:
+                        # From here on, don't use exceptions to interrupt the
+                        # thread as they would lead to "exception raised during
+                        # exception handling" errors. It is sufficient to set
+                        # `_stopRequested = True`, which will be honored in the
+                        # next run of the `while` loop.
+                        self._allowException = False
             except _InterruptThread:
+                # `self._func` call was interrupted
                 continue
             except Exception as e:
+                # An exception was raised in `self._func`
                 self.error.emit(e)
             else:
+                # No exception, also no `return` due to `_stopRequested`
                 self.finished.emit(result)
-            finally:
-                with self._callCondition:
-                    if self._args is None or self._kwargs is None:
-                        self._busy = False
-                        self.busyChanged.emit()
