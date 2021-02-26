63cd18d10771d1a708e608baeef7603968f709bb
Lukas Schrangl <schrangl@iap.tuwien.ac.at>
2021-02-26 21:46:47 +0100

    gui.BatchWorker: Let garbage collector take care of ThreadWorker
    When explicitly calling destroyLater(), the thread may not be finished
    yet and try to access already destroyed C++ object upon clean-up.

--- sdt/gui/batch_worker.py
+++ sdt/gui/batch_worker.py
@@ -182,7 +182,6 @@ class BatchWorker(QtQuick.QQuickItem):
         if self._worker is None:
             return
         self._worker.enabled = False
-        self._worker.deleteLater()
         self._worker = None
 
     def _nextCall(self):
