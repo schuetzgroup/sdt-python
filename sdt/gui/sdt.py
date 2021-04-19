1dcbc2db0d98789ff0521c9b801d43a783c1d8cd
Lukas Schrangl <lukas.schrangl@tuwien.ac.at>
2021-04-19 13:43:48 +0200

    gui.Sdt: Add `parentUrl` method

--- sdt/gui/sdt.py
+++ sdt/gui/sdt.py
@@ -3,12 +3,18 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import enum
+from pathlib import Path
 
 from PyQt5 import QtCore, QtQml
 
 
 class Sdt(QtCore.QObject):
+    """Collection of enums and functions
+
+    similar to Qt's ``Qt`` namespace.
+    """
     class WorkerStatus(enum.IntEnum):
+        """Status of a (thread/process) worker"""
         Idle = 0
         Working = enum.auto()
         Error = enum.auto()
@@ -30,6 +36,21 @@ class Sdt(QtCore.QObject):
         File name
         """
         return u.toLocalFile()
+
+    @QtCore.pyqtSlot(QtCore.QUrl, result=QtCore.QUrl)
+    def parentUrl(self, u: QtCore.QUrl) -> QtCore.QUrl:
+        """Get URL of directory containing `u`
+
+        Parameters
+        ----------
+        u
+            URL to get parent directory for
+
+        Returns
+        -------
+        URL to `u`'s parent
+        """
+        return QtCore.QUrl.fromLocalFile(str(Path(u.toLocalFile()).parent))
 
 
 QtQml.qmlRegisterType(Sdt, "SdtGui.Templates", 0, 1, "Sdt")
