a2a4bc13dfd345a7ccc928b00d433bb5a7e9ab13
Lukas Schrangl <lukas.schrangl@tuwien.ac.at>
2023-06-09 15:41:58 +0200

    gui.__main__: Adapt to recent changes

--- sdt/gui/__main__.py
+++ sdt/gui/__main__.py
@@ -14,14 +14,16 @@ app = QtWidgets.QApplication(sys.argv)
 app.setOrganizationName("schuetzgroup")
 app.setOrganizationDomain("biophysics.iap.tuwien.ac.at")
 app.setApplicationName("qml_wrapper")
-app.setApplicationVersion("0.1")
+app.setApplicationVersion("0.2")
 
 ap = argparse.ArgumentParser(description="Display QML item in a window")
 ap.add_argument("type", help="QML type to show in window")
 args = ap.parse_args()
 
 win = Window(args.type)
+win.create()
 if win.status_ == Window.Status.Error:
     sys.exit(1)
+win.window_.setTitle(args.type)
 
-sys.exit(app.exec_())
+sys.exit(app.exec())
