69939d517d4cc9792e1518bc5b03b45a2c1bca5f
Lukas Schrangl <lukas.schrangl@tuwien.ac.at>
2023-06-09 16:04:42 +0200

    gui._mpl_path_elements: Fixes and tests

--- sdt/gui/_mpl_path_elements.py
+++ sdt/gui/_mpl_path_elements.py
@@ -2,11 +2,15 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-from typing import Dict, List, Union
+import math
+from typing import Dict, List
 
 from PyQt5 import QtCore, QtQml
 import matplotlib as mpl
+import matplotlib.path
 import numpy as np
+
+from .qml_wrapper import SimpleQtProperty
 
 
 class MplPathElements(QtCore.QObject):
@@ -32,10 +36,10 @@ class MplPathElements(QtCore.QObject):
         self._width = 0.0
         self._height = 0.0
 
-    pathChanged = QtCore.pyqtSignal(QtCore.QVariant)
+    pathChanged = QtCore.pyqtSignal()
     """Path property was changed"""
 
-    @QtCore.pyqtProperty(QtCore.QVariant, notify=pathChanged)
+    @QtCore.pyqtProperty("QVariant", notify=pathChanged)
     def path(self) -> mpl.path.Path:
         """Input path"""
         return self._path
@@ -44,64 +48,57 @@ class MplPathElements(QtCore.QObject):
     def path(self, path: mpl.path.Path):
         if self._path == path:
             return
+        self._path = path
+
         bbox = path.get_extents()
         top_left = bbox.min
-        self._path = mpl.path.Path(path.vertices - top_left, path.codes)
+        verts = path.vertices - top_left
+        if path.codes is None:
+            codes = [mpl.path.Path.LINETO] * len(verts)
+            codes[0] = mpl.path.Path.MOVETO
+        else:
+            codes = path.codes
+
         elements = []
         idx = 0
-        while idx < len(self._path.vertices):
-            if path.codes is None:
-                code = (mpl.path.Path.MOVETO if idx == 0
-                        else mpl.path.Path.LINETO)
-            else:
-                code = self._path.codes[idx]
+        while idx < len(verts):
+            code = codes[idx]
             nPoints = mpl.path.Path.NUM_VERTICES_FOR_CODE[code]
-            elements.append({
-                "type": int(code),
-                "points": (self._path.vertices[idx:idx+nPoints]
-                           .flatten().tolist())})
+
+            if code == mpl.path.Path.STOP:
+                break
+
+            if code == mpl.path.Path.CLOSEPOLY:
+                code = mpl.path.Path.LINETO
+                v = verts[0]
+            else:
+                v = verts[idx:idx+nPoints].flatten()
+            elements.append({"type": int(code), "points": v.tolist()})
             idx += nPoints
+
         self._elements = elements
-        self._x, self._y = top_left
-        self._width, self._height = bbox.size
-        self.xChanged.emit(self._x)
-        self.yChanged.emit(self._y)
-        self.widthChanged.emit(self._width)
-        self.heightChanged.emit(self._height)
-        self.pathChanged.emit(path)
-        self.elementsChanged.emit(elements)
 
-    elementsChanged = QtCore.pyqtSignal(list)
-    """Path elements changed"""
+        self.pathChanged.emit()
+        self.elementsChanged.emit()
+        if not math.isclose(top_left[0], self._x):
+            self._x = top_left[0]
+            self.xChanged.emit()
+        if not math.isclose(top_left[1], self._y):
+            self._y = top_left[1]
+            self.yChanged.emit()
+        if not math.isclose(bbox.size[0], self._width):
+            self._width = bbox.size[0]
+            self.widthChanged.emit()
+        if not math.isclose(bbox.size[1], self._height):
+            self._height = bbox.size[1]
+            self.heightChanged.emit()
 
-    @QtCore.pyqtProperty(list, notify=elementsChanged)
-    def elements(self) -> List[Dict[str, Union[int, List[float]]]]:
-        return self._elements
-
-    xChanged = QtCore.pyqtSignal(float)
-
-    @QtCore.pyqtProperty(float, notify=xChanged)
-    def x(self) -> float:
-        return self._x
-
-    yChanged = QtCore.pyqtSignal(float)
-
-    @QtCore.pyqtProperty(float, notify=yChanged)
-    def y(self) -> float:
-        return self._y
-
-    widthChanged = QtCore.pyqtSignal(float)
-
-    @QtCore.pyqtProperty(float, notify=widthChanged)
-    def width(self) -> float:
-        return self._width
-
-    heightChanged = QtCore.pyqtSignal(float)
-
-    @QtCore.pyqtProperty(float, notify=heightChanged)
-    def height(self) -> float:
-        return self._height
+    elements: List[Dict] = SimpleQtProperty(list, readOnly=True)
+    x: float = SimpleQtProperty(float, readOnly=True)
+    y: float = SimpleQtProperty(float, readOnly=True)
+    width: float = SimpleQtProperty(float, readOnly=True)
+    height: float = SimpleQtProperty(float, readOnly=True)
 
 
-QtQml.qmlRegisterType(MplPathElements, "SdtGui.Templates", 0, 1,
+QtQml.qmlRegisterType(MplPathElements, "SdtGui.Templates", 0, 2,
                       "MplPathElements")
