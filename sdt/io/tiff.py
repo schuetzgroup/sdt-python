60ac9573a46723269bd039fa6690a0c897ee6ba8
Lukas Schrangl <lukas.schrangl@tuwien.ac.at>
2023-10-05 11:51:24 +0200

    io.tiff: black code style

--- sdt/io/tiff.py
+++ sdt/io/tiff.py
@@ -13,27 +13,42 @@ from typing import Iterable, List, Mapping, Union
 import numpy as np
 import tifffile
 
-try:
+with contextlib.suppress(ImportError):
     from . import yaml
-except ImportError:
-    yaml = None
 
 
 _logger = logging.getLogger(__name__)
 
 
 redundant_tiff_metadata: List = [
-    "compression", "frame_no", "is_fluoview", "is_imagej", "is_micromanager",
-    "is_mdgel", "is_mediacy", "is_nih", "is_ome", "is_reduced", "is_shaped",
-    "is_stk", "is_tiled", "predictor", "resolution", "resolution_unit"]
+    "compression",
+    "frame_no",
+    "is_fluoview",
+    "is_imagej",
+    "is_micromanager",
+    "is_mdgel",
+    "is_mediacy",
+    "is_nih",
+    "is_ome",
+    "is_reduced",
+    "is_shaped",
+    "is_stk",
+    "is_tiled",
+    "predictor",
+    "resolution",
+    "resolution_unit",
+]
 """Metadata that should not be saved in the image description since it is
 actually stored elsewhere.
 """
 
 
-def save_as_tiff(filename: Union[str, Path], frames: Iterable[np.ndarray],
-                 metadata: Union[None, Iterable[Mapping], Mapping] = None,
-                 contiguous: bool = True):
+def save_as_tiff(
+    filename: Union[str, Path],
+    frames: Iterable[np.ndarray],
+    metadata: Union[None, Iterable[Mapping], Mapping] = None,
+    contiguous: bool = True,
+):
     """Write a sequence of images to a TIFF stack
 
     If the items in `frames` contain a dict named `metadata`, an attempt to
@@ -60,8 +75,7 @@ def save_as_tiff(filename: Union[str, Path], frames: Iterable[np.ndarray],
         metadata = [metadata]
 
     with tifffile.TiffWriter(filename) as tw:
-        for f, md in zip(frames, itertools.chain(metadata,
-                                                 itertools.repeat({}))):
+        for f, md in zip(frames, itertools.chain(metadata, itertools.repeat({}))):
             desc = None
             dt = None
 
@@ -69,9 +83,10 @@ def save_as_tiff(filename: Union[str, Path], frames: Iterable[np.ndarray],
                 # PIMS and old imageio added metadata to frames
                 for k in "meta", "metadata":
                     with contextlib.suppress(
-                            AttributeError,  # no metadata
-                            TypeError, ValueError,  # dict() fail
-                            ):
+                        AttributeError,  # no metadata
+                        TypeError,
+                        ValueError,  # dict() fail
+                    ):
                         md = dict(getattr(f, k))
                         break
             if md:
@@ -87,8 +102,12 @@ def save_as_tiff(filename: Union[str, Path], frames: Iterable[np.ndarray],
                 try:
                     desc = yaml.safe_dump(md)
                 except Exception:
-                    _logger.error(
-                        f"{filename}: Failed to serialize metadata to YAML")
+                    _logger.error(f"{filename}: Failed to serialize metadata to YAML")
 
-            tw.write(f, software="sdt.io", description=desc, datetime=dt,
-                     contiguous=contiguous)
+            tw.write(
+                f,
+                software="sdt.io",
+                description=desc,
+                datetime=dt,
+                contiguous=contiguous,
+            )
